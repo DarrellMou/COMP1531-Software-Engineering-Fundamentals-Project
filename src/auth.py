bb3b457687c3a998d52f72d2eefd9fbe7b4e378c
winston <winstonlin@winstondeMacBook-Air.local>
2021-02-28 18:36:27 +1100

    auth_register_v1 done, but still need a way/source for auth_user_id, which is the return value of auth_register_v1

--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,11 @@
 # need a dictionary to match credentials with actual reference(auth_user_id) 
 from src.error import InputError 
 import re
+import itertools
 
 # temporary userinfo, the database
 userinfo = {
-    'adminemail@domain.com' : {'passwd' : '123456', 'auth_user_id' : '999'} # list or dictionary is ok, but not set
+    'adminemail@domain.com' : {'passwd' : '123456', 'handle' : 'ahandle', 'auth_user_id' : '999'} # list or dictionary is ok, but not set
 }
 
 def auth_login_v1(email, password):				
@@ -17,7 +18,31 @@ def auth_login_v1(email, password):
     raise InputError
 
 def auth_register_v1(email, password, name_first, name_last):
-    return 1
+    if(auth_email_format(email) == False):
+        raise InputError
+    elif(email in userinfo.keys()):
+        raise InputError
+    elif(len(password) < 6):
+        raise InputError
+    elif(len(name_first) > 50 or len(name_first) < 1 or len(name_last) > 50 or len(name_last) < 1):
+        raise InputError
+    
+    # generate handle and add to userinfo
+    new_handle = name_first.lower() + name_last.lower()
+    if(len(new_handle) > 20):
+        handle = new_handle[0:19]
+
+    new_auth_user_id = 1234 # TODO: find a new way to generate 
+
+    if(any(new_handle == userinfo[it]['handle'] for it in userinfo.keys())):
+        for epilogue in itertools.count(0, 1):
+            if(not any((new_handle + str(epilogue)) == userinfo[it]['handle'] for it in userinfo.keys())):
+                userinfo[email] = {'passwd' : password, 'handle' : new_handle + str(epilogue), 'auth_user_id' : new_auth_user_id}
+                return {'auth_user_id' : new_auth_user_id}
+    else:   # unique handle, add straght away 
+        userinfo[email] = {'passwd' : password, 'handle' : new_handle, 'auth_user_id'     : new_auth_user_id}
+        return {'auth_user_id' : new_auth_user_id}
+
 
 # checks if email address has valid format, if so returns true
 def auth_email_format(email):
