db879da5a8e3be3ab684b1fd3d5aec4fb66249ea
winston <winstonlin@winstondeMacBook-Air.local>
2021-02-28 18:29:46 +1100

    fixed the test that expects an exception(InputError) to be thrown from auth_login_v1, and added some tests for auth_register_v1

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,7 +1,7 @@
 import pytest
 
-from src.auth import auth_login_v1, auth_email_format
 from src.error import InputError
+from src.auth import auth_login_v1, auth_email_format, auth_register_v1
 
 def test_auth_email_format():
     assert auth_email_format('123@gmailcom') == False, 'invalid email format'
@@ -10,8 +10,14 @@ def test_auth_email_format():
 
 def test_auth_login_v1():
     assert auth_login_v1('adminemail@domain.com', '123456') == '999', 'login function broken'
-    try:
-        auth_login_v1('adminemail@domain.com', '123457')
-    except InputError:
-        print('Error: auth_login_v1')
+    with pytest.raises(InputError):
+    	auth_login_v1('123456789@@gmail.com', '123456') # invalid email format 
+    	auth_login_v1('adminemail@domain.com', '123456off')	# valid format but password doesn't match
         
+def test_auth_register_v1():
+    assert auth_register_v1('sampleemail1@gmail.com', 'password1', 'test', 'user1') == {'auth_user_id' : 1234}
+    with pytest.raises(InputError):
+        auth_register_v1('sampleemail1gmail.com', 'password1', 'test', 'user1')
+        auth_register_v1('sampleemail1@gmail.com', 'password1', 'test', 'user1')
+        auth_register_v1('sampleemail2@gmail.com', 'passwo', 'test', 'user1')
+        auth_register_v1('sampleemail2@gmail.com', 'passwo', '', 'user1')
