80d657079fa2362fb474185837764475da3ec907
Nikki <z5260192@ad.unsw.edu.au>
2021-03-03 10:19:43 +1100

    Added code to channels_create_v1

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,8 @@
+import uuid
+
+from error import InputError, AccessError
+from data import retrieve_data
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -19,6 +24,24 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
+
+    data = retrieve_data
+
+    # error when creating a channel name longer than 20 characters
+    if len(name) > 20:
+        raise InputError(description = "Channel name cannot be longer than 20 characters")
+
+    # creates channel object
+    channel_new = {
+        'channel_id' : int(uuid.uuid1()) # make a int(UUID) based on the host ID and current time
+        'name' : name,  
+        'is_public' : is_public,  
+        'owner' : auth_user_id
+    }
+
+    # appends the new channel to the list of channels
+    data["channels"].append(channel_new)
+
     return {
-        'channel_id': 1,
+        'channel_id': channel_new['channel_id']
     }
