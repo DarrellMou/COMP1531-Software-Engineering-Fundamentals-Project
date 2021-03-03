f7f7e1fdfb5230011440f3a6fd62e04d50408a87
Nikki <z5260192@ad.unsw.edu.au>
2021-03-03 22:52:56 +1100

    Added access error code and added variables to channel object

--- src/channels.py
+++ src/channels.py
@@ -25,22 +25,33 @@ def channels_listall_v1(auth_user_id):
 
 def channels_create_v1(auth_user_id, name, is_public):
 
-    data = retrieve_data()
+    data = retrieve_data
 
     # error when creating a channel name longer than 20 characters
     if len(name) > 20:
         raise InputError(description = "Channel name cannot be longer than 20 characters")
+
+    # error when invalid auth_user_id
+    curr_user = {}
+    for user in data['users']:
+        if user['auth_user_id'] == auth_user_id:
+            curr_user = user
+    if curr_user == {}:
+        raise AccessError(description = "Invalid auth_user_id")
+        
 
     # creates channel object
     channel_new = {
         'channel_id' : int(uuid.uuid1()), # make a int(UUID) based on the host ID and current time
         'name' : name,  
         'is_public' : is_public,  
-        'owner' : auth_user_id
+        'owner' : [],
+        'members' : [],
+        'messages' : []
     }
 
     # appends the new channel to the list of channels
-    data["channels"].append(channel_new)
+    data['channels'].append(channel_new)
 
     return {
         'channel_id': channel_new['channel_id']
