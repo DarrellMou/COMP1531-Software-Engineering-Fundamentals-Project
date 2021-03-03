d5ff87ca4584732e01df859ea0c085604187095d
Nikki <z5260192@ad.unsw.edu.au>
2021-03-03 21:28:36 +1100

    Separated tests

--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -4,24 +4,31 @@ from src.auth import auth_register_v1
 from src.channels import channels_create_v1
 from src.error import InputError, AccessError
 
-def test_channels_create_v1():
-    
-    ## SET-UP USER ## 
-
-    # owner
+def setup():
     a_u_id_1 = auth_register_v1("bye@teams.com", "asdfg", "Brendan", "Ye")
-    
-    ##             ##
 
+# error when creating a channel with an invalid auth_user_id
+def test_channels_create_access_error():
+    with pytest.raises(AccessError):
+        channels_create_v1("invalid a_u_id", "Channel", True)
+
+# error when creating a channel name longer than 20 characters
+def test_channels_create_input_error():
+    with pytest.raises(InputError):
+        channels_create_v1(a_u_id_1, "This is longer than 20", True)
+
+# assert channel id is an integer
+def test_channels_create_return_value():
+    channel_id_1 = channels_create_v1(a_u_id_1, "Public Channel", True)
+    assert isinstance(channel_id_1['channel_id'], int)
+
+'''
     # owner creating a public and private channel
     channel_id_1 = channels_create_v1(a_u_id_1, "Public Channel", True)
     channel_id_2 = channels_create_v1(a_u_id_1, "Private Channel", False)
 
-    # error when creating a channel name longer than 20 characters
-    with pytest.raises(InputError):
-        channels_create_v1(a_u_id_1, "This is longer than 20", True)
+'''
+    
 
-    # error when creating a channel with an invalid auth_user_id
-    with pytest.raises(AccessError):
-        channels_create_v1("invalid a_u_id", "Channel", True)
+    
 
