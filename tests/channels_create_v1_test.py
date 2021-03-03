d3133e36ad715b02958efd7286abe861bad5a205
Nikki <z5260192@ad.unsw.edu.au>
2021-03-03 22:51:16 +1100

    Added another test and changed user input format

--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -1,34 +1,61 @@
 import pytest
 
-from src.auth import auth_register_v1
+from src.auth import auth_register_v1`
+from src.channel import channel_join_v1
 from src.channels import channels_create_v1
 from src.error import InputError, AccessError
 
-def setup():
-    a_u_id_1 = auth_register_v1("bye@teams.com", "asdfg", "Brendan", "Ye")
+def setup_user():
+    a_u_id_1 = auth_register_v1('user1@email.com', 'User1_pass!', 'user1_first', 'user1_last')
+    a_u_id_2 = auth_register_v1('user2@email.com', 'User2_pass!', 'user2_first', 'user2_last')
+    a_u_id_3 = auth_register_v1('user3@email.com', 'User3_pass!', 'user3_first', 'user3_last')
+    a_u_id_4 = auth_register_v1('user4@email.com', 'User4_pass!', 'user4_first', 'user4_last')
+    a_u_id_5 = auth_register_v1('user5@email.com', 'User5_pass!', 'user5_first', 'user5_last')
+
+    return {
+        'user1' : a_u_id_1,
+        'user2' : a_u_id_2,
+        'user3' : a_u_id_3,
+        'user4' : a_u_id_4,
+        'user5' : a_u_id_5
+    }
 
 # error when creating a channel with an invalid auth_user_id
 def test_channels_create_access_error():
+
     with pytest.raises(AccessError):
         channels_create_v1("invalid a_u_id", "Channel", True)
 
 # error when creating a channel name longer than 20 characters
 def test_channels_create_input_error():
+    users = setup_user()
+
+    # public channel with namesize > 20 characters
     with pytest.raises(InputError):
-        channels_create_v1("a_u_id_1", "This is longer than 20", True)
+        channels_create_v1(users['user1'], "This is longer than 20", True)
+
+    # private channel with namesize > 20 characters
+    with pytest.raises(InputError):
+        channels_create_v1(users['user1'], "This is longer than 20", False)
 
 # assert channel id is an integer
 def test_channels_create_return_value():
-    channel_id_1 = channels_create_v1("a_u_id_1", "Public Channel", True)
+    users = setup_user()
+
+    channel_id_1 = channels_create_v1(users['user1'], "Public Channel", True)
     assert isinstance(channel_id_1['channel_id'], int)
 
-'''
-    # owner creating a public and private channel
-    channel_id_1 = channels_create_v1(a_u_id_1, "Public Channel", True)
-    channel_id_2 = channels_create_v1(a_u_id_1, "Private Channel", False)
+# test private channel 
+def test_channels_create_private():
+    users = setup_user()
 
-'''
-    
+    channel_id_2 = channels_create_v1(users['user1'], "Private Channel", False)
+
+    # ensure a non-member cannot join private channel
+    with pytest.raises(AccessError):
+        channel_join_v1(users['user2'], channel_id_2)
+
+
 
     
 
