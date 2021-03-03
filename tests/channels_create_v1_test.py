f0deee05366336a32a6e3aa861c5d269b97933b9
Nikki <z5260192@ad.unsw.edu.au>
2021-03-03 11:18:25 +1100

    Updated basic tests

--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -1,19 +1,27 @@
 import pytest
 
-from src.auth import channels_create_v1
-from src.error import InputError
+from src.auth import auth_register_v1
+from src.channels import channels_create_v1
+from error import InputError, AccessError
 
 def test_channels_create_v1():
-    assert channels_create_v1(auth_user_id, name, is_public) == channel_id
+    
+    ## SET-UP USER ## 
 
-'''
-    assert auth_email_format('123@gmailcom') == False, 'invalid email format'
-    assert auth_email_format('jsfdsfdsds123.con') == False, 'invalid email format'
-    assert auth_email_format('myvalidemail@yahoogmail.com') == True, 'valid email format'
+    # owner
+    a_u_id_1 = auth_register_v1("bye@teams.com", "asdfg", "Brendan", "Ye")
+    
+    ##             ##
 
-def test_auth_login_v1():
-    assert auth_login_v1('adminemail@domain.com', '123456') == '999', 'login function broken'
-    try:
-        auth_login_v1('adminemail@domain.com', '123457')
-    except InputError:
-        print('Error: auth_login_v1')
+    # owner creating a public and private channel
+    channel_id_1 = channels_create_v1(a_u_id_1, "Public Channel", True)
+    channel_id_2 = channels_create_v1(a_u_id_1, "Private Channel", False)
+
+    # error when creating a channel name longer than 20 characters
+    with pytest.raises(InputError):
+        channels_create_v1(a_u_id_1, "This is longer than 20", True)
+
+    # error when creating a channel with an invalid auth_user_id
+    with pytest.raises(AccessError):
+        channels_create_v1("invalid a_u_id", "Channel", True)
+
