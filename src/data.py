fa0a43ca61a12d5e0be165444b44b223152ca4d4
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-03-03 13:21:07 +1100

    Changed format for data.py, more readable

--- src/data.py
+++ src/data.py
@@ -1,52 +1,42 @@
 data = {
-    'users' : [
-        {
-            'id' : 1,
+    'users' : {
+        'auth_user_id1' : {
+                'name_first' : 'first_name',
+                'name_last' : 'last_name',
+                'email' : 'random@hotmail.com',
+                'password' : 'password123',
+        },
+        'auth_user_id2' : {
             'name_first' : 'first_name',
             'name_last' : 'last_name',
             'email' : 'random@hotmail.com',
             'password' : 'password123',
-            'auth_user_id' : 'whatever',
         },
-        {
-            'id' : 2,
+        'auth_user_id3' : {
             'name_first' : 'first_name',
             'name_last' : 'last_name',
             'email' : 'random@hotmail.com',
             'password' : 'password123',
-            'auth_user_id' : 'whatever',
         },
-        {
-            'id' : 3,
+        'auth_user_id4' : {
             'name_first' : 'first_name',
             'name_last' : 'last_name',
             'email' : 'random@hotmail.com',
             'password' : 'password123',
-            'auth_user_id' : 'whatever',
         },
-        {
-            'id' : 4,
-            'name_first' : 'first_name',
-            'name_last' : 'last_name',
-            'email' : 'random@hotmail.com',
-            'password' : 'password123',
-            'auth_user_id' : 'whatever',
+    },
+    'channels' : {
+        'channel_id1' : {
+                'name' : 'channel1',
+                'is_public' : False,
+                'owner_members' : ['id1', 'id2'],
+                'members' : ['id3', 'id4'],
         },
-    ],
-    'channels' : [
-        {
-            'id' : 1,
-            'name' : 'channel1',
-            'is_public' : False,
-            'owner_members' : ['id1', 'id2'],
-            'members' : ['id3', 'id4'],
+        'channel_id2' : {
+                'name' : 'channel2',
+                'is_public' : True,
+                'owner_members' : ['id1', 'id2'],
+                'members' : ['id3', 'id4'],
         },
-        {
-            'id': 2,
-            'name' : 'channel2',
-            'is_public' : True,
-            'owner_members' : ['id1', 'id2'],
-            'members' : ['id3', 'id4'],
-        },
-    ],
+    },
 }
