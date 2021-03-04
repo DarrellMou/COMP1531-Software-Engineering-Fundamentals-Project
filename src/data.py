ba42a7e5628c62f3475ccee3055351bae9aa074b
Brendan Ye <z5254583@ad.unsw.edu.au>
2021-03-04 20:25:31 +1100

    Added some comments to make the file more clear

--- src/data.py
+++ src/data.py
@@ -1,11 +1,13 @@
 # Updated data file so we have a clear data section
 
+# Data dictionary which will be used throughout the project
 data = {
     "users" : [],
     "channels" : [],
     "messages" : []
 }
 
+# Function to reset the data to default (assists in testing)
 def reset_data():
     global data
     data = {
