abc0f62421913c3ef37707c03a403244b40ff3a8
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-03-04 20:03:06 +1100

    Added write_to_data function, need to discuss more

--- src/other.py
+++ src/other.py
@@ -12,3 +12,34 @@ def search_v1(auth_user_id, query_str):
             }
         ],
     }
+
+def write_into_data(data):
+    user_string = ''
+    for user in data['users']:
+        user_string += f"""    '{user}' : {'{'}
+                'name_first' : '{data['users'][user]['name_first']}',
+                'name_last' : '{data['users'][user]['name_last']}',
+                'email' : '{data['users'][user]['email']}',
+                'password' : '{data['users'][user]['password']}',
+            {'},'}
+        """
+
+    channel_string = ''
+    for channel in data['channels']:
+        channel_string += f"""    '{channel}' : {'{'}
+                'name' : '{data['channels'][channel]['name']}',
+                'is_public' : {data['channels'][channel]['is_public']},
+                'owners' : {data['channels'][channel]['owners']},
+                'members' : {data['channels'][channel]['members']},
+            {'},'}
+        """
+    data_string = f'''data = {'{'}
+        'users' : {'{'}
+        {user_string}{'},'}
+        'channels' : {'{'}
+        {channel_string}{'},'}
+    {'}'}'''
+
+    f = open("data.py", "w")
+    f.write(data_string)
+    f.close()
