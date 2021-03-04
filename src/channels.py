5def3669d47a824f55cb9dd2e4bb64c5d03c2bad
Nikki <z5260192@ad.unsw.edu.au>
2021-03-04 15:49:16 +1100

    Added code

--- src/channels.py
+++ src/channels.py
@@ -14,13 +14,30 @@ def channels_list_v1(auth_user_id):
     }
 
 def channels_listall_v1(auth_user_id):
+
+    data = retrieve_data()
+    
+    # error when invalid auth_user_id
+    curr_user = {}
+    # find auth_user_id in data
+    for user in data['users']:
+        if user['auth_user_id'] == auth_user_id:
+            curr_user = user
+    if curr_user == {}:
+        raise AccessError("Invalid auth_user_id")
+
+    # list of all channels
+    channel_listall = []
+
+    for channel in data['channels']:
+        channel_details = {
+            'channel_id' : channel['channel_id'],
+            'name' : channel['name']
+        }
+        channel_listall.append(channel_details)
+
     return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
+        'channels': channel_listall
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
