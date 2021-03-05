38f98c63689a444e285685af8b9f1652fe05f8b8
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-03-05 15:14:44 +1100

    Fixed channel_invite

--- src/channel.py
+++ src/channel.py
@@ -10,59 +10,51 @@ from src.channels import channels_create_v1
 
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     # Checks for any errors involving parameters
-    if not(any(channel == channel_id['channel_id'] for channel in data['channels'])): raise InputError
-    if not(any(user == u_id['auth_user_id'] for user in data['users'])): raise InputError
-    if not(any(user == auth_user_id['auth_user_id'] for user in data['channels'][channel_id['channel_id']]['owner_members'] + data['channels'][channel_id['channel_id']]['all_members'])): raise AccessError
+    if not(any(channel == channel_id for channel in data['channels'])): raise InputError
+    if not(any(user == u_id for user in data['users'])): raise InputError
+    if not(any(user == auth_user_id for user in data['channels'][channel_id]['owner_members'] + data['channels'][channel_id]['all_members'])): raise AccessError
 
     # Appends new user to given channel
     # Assume no duplicate entries allowed
-    if not(any(user == u_id['auth_user_id'] for user in data['channels'][channel_id['channel_id']]['owner_members'] + data['channels'][channel_id['channel_id']]['all_members'])):
-        data['channels'][channel_id['channel_id']]['all_members'].append(u_id['auth_user_id'])
+    # Assume no inviting themselves
+    if not(any(user == u_id for user in data['channels'][channel_id]['owner_members'] + data['channels'][channel_id]['all_members'])):
+        data['channels'][channel_id]['all_members'].append(u_id)
 
 
 def channel_details_v1(auth_user_id, channel_id):
     # Checks for any errors involving parameters
-    if not(any(channel == channel_id['channel_id'] for channel in data['channels'])): raise InputError
-    if not(any(user == auth_user_id['auth_user_id'] for user in data['channels'][channel_id['channel_id']]['owner_members'] + data['channels'][channel_id['channel_id']]['all_members'])): raise AccessError
+    if not(any(channel == channel_id for channel in data['channels'])): raise InputError
+    if not(any(user == auth_user_id for user in data['channels'][channel_id]['owner_members'] + data['channels'][channel_id]['all_members'])): raise AccessError
 
     # Creates list with necessary data
-    name = data['channels'][channel_id['channel_id']]['name']
-    owners = data['channels'][channel_id['channel_id']]['owner_members']
-    members = data['channels'][channel_id['channel_id']]['all_members']
+    name = data['channels'][channel_id]['name']
+    owners = data['channels'][channel_id]['owner_members']
+    members = data['channels'][channel_id]['all_members']
 
-    string = ''
-    string += f''''name': {name},\n'''
-    string += "'owner_members': ["
-    # Printing function
-    #print('name: ' + name)
-    #print('owner_members: [')
-    i = 0
-    # Iterates through owner members, and prints
+    details_dict = {}
+    details_dict['name'] = name
+    details_dict['owner_members'] = []
+    details_dict['all_members'] = []
+
+    tmp_list = []
     for owner in owners:
-        string += f'''
-    {'{'}
-        u_id: {owners[i]},
-        name_first: {data['users'][owner]['name_first']},
-        name_last: {data['users'][owner]['name_last']},
-    {'},'}'''
-        i += 1
-    string += f'\n],'
+        tmp_dict = {}
+        tmp_dict['u_id'] = owner
+        tmp_dict['name_first'] = data['users'][owner]['name_first']
+        tmp_dict['name_last'] = data['users'][owner]['name_last']
+        tmp_list.append(tmp_dict)
+    details_dict['owner_members'] = tmp_list
 
-    string += f'\n'
-    string += "'all_members': ["
-    # Iterates through other members, and prints
-    #print('all_members: [')
-    i = 0
+    tmp_list = []
     for member in members:
-        string += f'''
-    {'{'}
-        u_id: {members[i]},
-        name_first: {data['users'][member]['name_first']},
-        name_last: {data['users'][member]['name_last']},
-    {'},'}'''
-        i += 1
-    string += f'\n],'
-    return string
+        tmp_dict = {}
+        tmp_dict['u_id'] = member
+        tmp_dict['name_first'] = data['users'][member]['name_first']
+        tmp_dict['name_last'] = data['users'][member]['name_last']
+        tmp_list.append(tmp_dict)
+    details_dict['all_members'] = tmp_list
+
+    return details_dict
 
 def channel_messages_v1(auth_user_id, channel_id, start):
     return {
@@ -93,5 +85,3 @@ def channel_addowner_v1(auth_user_id, channel_id, u_id):
 def channel_removeowner_v1(auth_user_id, channel_id, u_id):
     return {
     }
-
-print(data)
