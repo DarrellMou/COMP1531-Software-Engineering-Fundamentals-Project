3656c4417c4699206c93d6bd6163940a6f4753fb
Nikki <z5260192@ad.unsw.edu.au>
2021-03-05 14:56:19 +1100

    Working code

--- src/channels.py
+++ src/channels.py
@@ -9,7 +9,7 @@ def channels_list_v1(auth_user_id):
         	{
         		'channel_id': 1,
         		'name': 'My Channel',
-        	}
+        	},
         ],
     }
 
@@ -19,7 +19,7 @@ def channels_listall_v1(auth_user_id):
         	{
         		'channel_id': 1,
         		'name': 'My Channel',
-        	}
+        	},
         ],
     }
 
@@ -31,38 +31,38 @@ def channels_create_v1(auth_user_id, name, is_public):
     if len(name) > 20:
         raise InputError("Channel name cannot be longer than 20 characters")
 
-    # error when invalid auth_user_id
+    # AccessError occurs when input isauth_user_id
     curr_user = {}
     for user in data['users']:
-        if user['auth_user_id'] == auth_user_id['auth_user_id']:
+        if user == auth_user_id:
             curr_user = user
     if curr_user == {}:
         raise AccessError("Invalid auth_user_id")
-        
-    # creates channel object
-    channel_new = {
-        'channel_id' : int(uuid.uuid1()), # make a int(UUID) based on the host ID and current time
-        'name' : name,  
-        'is_public' : is_public,  
-        'owner_members': [
-            {
-                'auth_user_id': curr_user['auth_user_id'],
-                'name_first': curr_user['name_first'],
-                'name_last': curr_user['name_last'],
-            }
-        ],
-        'all_members': [
-            {
-                'auth_user_id': curr_user['auth_user_id'],
-                'name_first': current_user['name_first'],
-                'name_last': current_user['name_last'],
-            }
-        ],
+
+    channel_id = int(uuid.uuid1())
+
+    data['channels'][channel_id] = {
+        'name' : name, 
+        'is_public' : is_public, 
+        'owner_members' : auth_user_id,
+        'all_members' : auth_user_id,
         'messages' : [],
-    }
-    # appends the new channel to the list of channels
-    data['channels'].append(channel_new)
+    }   
 
     return {
-        'channel_id': channel_new['channel_id']
+        'channel_id': channel_id
     }
+
+'''
+# assert channel id is an integer
+if __name__ == '__main__' :
+
+    global data
+    
+    user1 = auth_register_v1('user1@email.com', 'User1_pass!', 'user1_first', 'user1_last')
+    print(user1)
+
+    print(     ) 
+    channel_id1 = channels_create_v1(user1['auth_user_id'], "Public Channel", True)
+    print(channel_id1)
+'''
