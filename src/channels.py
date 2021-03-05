58c3d73ae773f62b175804d54884878cb8a73e45
KelNoMel <68493100+KelNoMel@users.noreply.github.com>
2021-03-06 00:32:26 +1100

    channels_list draft
    Basic draft of channels_list, most likely going to be spaghetti code and fail the pipe. Just pushing for reference and review

--- src/channels.py
+++ src/channels.py
@@ -1,12 +1,25 @@
 def channels_list_v1(auth_user_id):
-    return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
-    }
+    data = retrieve_data()
+
+    # No parameter errors
+    # List of channels
+    channel_ids = data['channels']
+
+    # Search through individual channels for specific user
+    for channel in channel_ids:
+        # Create a list of channel attributes
+        name = data['channels'][channel]['name']
+        owners = data['channels'][channel]['owners']
+        members = data['channels'][channel]['members']
+
+        #Look for positive match
+        for owner in owners:
+            if owner = auth_user_id:
+                print('channel_id: ' + channel '\nname: ' + name)
+        for member in members:
+            if member = auth_user_id:
+                print('channel_id: ' + channel '\nname: ' + name)
+
 
 def channels_listall_v1(auth_user_id):
     return {
--- src/data.py
+++ src/data.py
@@ -1,99 +1,75 @@
+# Iteration 1 test data
 data = {
-    'users' : [
-        {
-            'id' : 1,
-            'name_first' : 'first_name',
-            'name_last' : 'last_name',
-            'email' : 'random@hotmail.com',
-            'password' : 'password123',
-            'auth_user_id' : 1,
-
+    'users' : {
+        35746842521 : {
+            'name_first' : 'first_name1',
+            'name_last' : 'last_name1',
+            'email' : 'example1@hotmail.com',
+            'password' : 'password1',
+            'handle_str' : 'first_name1last_name',
         },
-        {
-            'id' : 2,
-            'name_first' : 'first_name',
-            'name_last' : 'last_name',
-            'email' : 'random@hotmail.com',
-            'password' : 'password123',
-            'auth_user_id' : 2,
+        11753764853 : {
+            'name_first' : 'first_name2',
+            'name_last' : 'last_name2',
+            'email' : 'example2@hotmail.com',
+            'password' : 'password2',
+            'handle_str' : 'first_name2last_name',
         },
-    ],
-    'channels' : [
-        {
-            'channel_id' : 1,
+    },
+    'channels' : {
+        14723573315 : {
             'name' : 'channel1',
             'is_public' : True,
-            'owner_members' : [
-                {
-                    'auth_user_id' : 1
-                    'name_first' : 'first_name'
-                    'name_last' : 'last_name'
-                }
-            ],
-            'all_members' : [
-                {
-                    'auth_user_id' : 1
-                    'name_first' : 'first_name'
-                    'name_last' : 'last_name'
-                },
-                {
-                    'auth_user_id' : 2
-                    'name_first' : 'first_name'
-                    'name_last' : 'last_name'
-                }
-            ],
+            'owner_members' : ['auth_user_id1'],
+            'all_members' : ['auth_user_id1', 'auth_user_id2'],
             'messages' : [
                 {
-                    'message_id' : 0
-                    'auth_user_id' : 1
-                    'message' : 'Hello World'
-                    'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)'
+                    'message_id': 4561328123,
+                    'u_id': 35746842521,
+                    'message': 'Hello World1',
+                    'time_created': 123416589,
                 },
                 {
-                    'message_id' : 1
-                    'auth_user_id' : 2
-                    'message' : 'Hello World2'
-                    'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)'
-                }
-            ],
+                    'message_id': 61510648893,
+                    'u_id': 11753764853,
+                    'message': 'Hello World2',
+                    'time_created': 123456789,
+                },
+            ]
         },
-        {
-            'channel_id' : 2,
+        31627643273 : {
             'name' : 'channel2',
-            'is_public' : False,
-            'owner_members' : [
-                {
-                    'auth_user_id' : 1
-                    'name_first' : 'first_name'
-                    'name_last' : 'last_name'
-                }
-            ],
-            'all_members' : [
-                {
-                    'auth_user_id' : 1
-                    'name_first' : 'first_name'
-                    'name_last' : 'last_name'
-                },
-                {
-                    'auth_user_id' : 2
-                    'name_first' : 'first_name'
-                    'name_last' : 'last_name'
-                }
-            ],
+            'is_public' : True,
+            'owner_members' : [123456789],
+            'all_members' : [12356789, 12389473129847],
             'messages' : [
                 {
-                    'message_id' : 0
-                    'auth_user_id' : 1
-                    'message' : 'Hello World'
-                    'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)'
+                    'message_id': 12354122383,
+                    'u_id': 35746842521,
+                    'message': 'Hello World1',
+                    'time_created': 45132806512,
                 },
                 {
-                    'message_id' : 1
-                    'auth_user_id' : 2
-                    'message' : 'Hello World2'
-                    'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)'
+                    'message_id': 123156231064,
+                    'u_id': 11753764853,
+                    'message': 'Hello World2',
+                    'time_created': 68741450315603,
                 }
             ],
         },
-    ],
+    },
 }
+
+
+# Function to reset the data to default (assists in testing)
+def reset_data():
+    global data
+    data = {
+        "users" : {},
+        "channels" : {}
+    }
+    return data
+
+def retrieve_data():
+    global data
+    return data
