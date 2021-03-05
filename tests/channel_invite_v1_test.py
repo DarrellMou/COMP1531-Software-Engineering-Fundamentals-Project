79ad34fbc6e04abeb73440058a02d1726ee325c9
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-03-05 15:40:45 +1100

    Fixed pytests

--- tests/channel_invite_v1_test.py
+++ tests/channel_invite_v1_test.py
@@ -20,18 +20,23 @@ def test_function():
     a_u_id2 = auth_register_v1('example2@hotmail.com', 'password2', 'first_name2', 'last_name2') #returns auth_user_id e.g.
     ch_id = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #returns channel_id e.g.
     channel_invite_v1(a_u_id1['auth_user_id'], ch_id['channel_id'], a_u_id2['auth_user_id'])
-    assert channel_details_v1(a_u_id2['auth_user_id'], ch_id['channel_id']) == {
+    assert channel_details_v1(a_u_id1['auth_user_id'], ch_id['channel_id']) == {
         'name': 'channel1',
         'owner_members': [
             {
-                'u_id': a_u_id1,
+                'u_id': a_u_id1['auth_user_id'],
                 'name_first': 'first_name1',
                 'name_last': 'last_name1',
             }
         ],
         'all_members': [
             {
-                'u_id': a_u_id2,
+                'u_id': a_u_id1['auth_user_id'],
+                'name_first': 'first_name1',
+                'name_last': 'last_name1',
+            },
+            {
+                'u_id': a_u_id2['auth_user_id'],
                 'name_first': 'first_name2',
                 'name_last': 'last_name2',
             }
@@ -40,44 +45,50 @@ def test_function():
 
 # Running channel_invite multiple times
 def test_multiple_runs():
-    data.clear()
-    auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1') #returns auth_user_id1 e.g.
-    auth_register_v1('example2@hotmail.com', 'password2', 'first_name2', 'last_name2') #returns auth_user_id2 e.g.
-    auth_register_v1('example3@hotmail.com', 'password3', 'first_name3', 'last_name3') #returns auth_user_id3 e.g.
-    auth_register_v1('example4@hotmail.com', 'password4', 'first_name4', 'last_name4') #returns auth_user_id4 e.g.
-    auth_register_v1('example5@hotmail.com', 'password5', 'first_name5', 'last_name5') #returns auth_user_id5 e.g.
-    channels_create_v1('auth_user_id1', 'channel1', True) #returns channel_id1 e.g.
-    channel_invite_v1('auth_user_id1', 'channel_id1', 'auth_user_id2')
-    channel_invite_v1('auth_user_id1', 'channel_id1', 'auth_user_id3')
-    channel_invite_v1('auth_user_id1', 'channel_id1', 'auth_user_id4')
-    channel_invite_v1('auth_user_id1', 'channel_id1', 'auth_user_id5')
-    assert channel_details_v1('auth_user_id2', 'channel_id1') == {
+    data['users'] = {}
+    data['channels'] = {}
+    a_u_id1 = auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1') #returns auth_user_id1 e.g.
+    a_u_id2 = auth_register_v1('example2@hotmail.com', 'password2', 'first_name2', 'last_name2') #returns auth_user_id2 e.g.
+    a_u_id3 = auth_register_v1('example3@hotmail.com', 'password3', 'first_name3', 'last_name3') #returns auth_user_id3 e.g.
+    a_u_id4 = auth_register_v1('example4@hotmail.com', 'password4', 'first_name4', 'last_name4') #returns auth_user_id4 e.g.
+    a_u_id5 = auth_register_v1('example5@hotmail.com', 'password5', 'first_name5', 'last_name5') #returns auth_user_id5 e.g.
+    ch_id = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #returns channel_id1 e.g.
+    channel_invite_v1(a_u_id1['auth_user_id'], ch_id['channel_id'], a_u_id2['auth_user_id'])
+    channel_invite_v1(a_u_id1['auth_user_id'], ch_id['channel_id'], a_u_id3['auth_user_id'])
+    channel_invite_v1(a_u_id1['auth_user_id'], ch_id['channel_id'], a_u_id4['auth_user_id'])
+    channel_invite_v1(a_u_id1['auth_user_id'], ch_id['channel_id'], a_u_id5['auth_user_id'])
+    assert channel_details_v1(a_u_id2['auth_user_id'], ch_id['channel_id']) == {
         'name': 'channel1',
         'owner_members': [
             {
-                'u_id': 1,
+                'u_id': a_u_id1['auth_user_id'],
                 'name_first': 'first_name1',
                 'name_last': 'last_name1',
             }
         ],
         'all_members': [
             {
-                'u_id': 2,
+                'u_id': a_u_id1['auth_user_id'],
+                'name_first': 'first_name1',
+                'name_last': 'last_name1',
+            },
+            {
+                'u_id': a_u_id2['auth_user_id'],
                 'name_first': 'first_name2',
                 'name_last': 'last_name2',
             },
             {
-                'u_id': 3,
+                'u_id': a_u_id3['auth_user_id'],
                 'name_first': 'first_name3',
                 'name_last': 'last_name3',
             },
             {
-                'u_id': 4,
+                'u_id': a_u_id4['auth_user_id'],
                 'name_first': 'first_name4',
                 'name_last': 'last_name4',
             },
             {
-                'u_id': 5,
+                'u_id': a_u_id5['auth_user_id'],
                 'name_first': 'first_name5',
                 'name_last': 'last_name5',
             },
@@ -86,27 +97,30 @@ def test_multiple_runs():
 
 # Channel_invite given channel id belonging to a non-existent channel
 def test_invalid_channel_id():
-    data.clear()
-    auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1') #returns auth_user_id1 e.g.
-    auth_register_v1('example2@hotmail.com', 'password2', 'first_name2', 'last_name2') #returns auth_user_id2 e.g.
+    data['users'] = {}
+    data['channels'] = {}
+    a_u_id1 = auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1') #returns auth_user_id1 e.g.
+    a_u_id2 = auth_register_v1('example2@hotmail.com', 'password2', 'first_name2', 'last_name2') #returns auth_user_id2 e.g.
     with pytest.raises(InputError):
-        channel_invite_v1('auth_user_id1', 'channel_id1', 'auth_user_id2')
+        channel_invite_v1(a_u_id1['auth_user_id'], 13637355236473, a_u_id2['auth_user_id'])
 
 # Channel_invite given user that does not exist
 def test_invalid_invited_user():
-    data.clear()
-    auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1') #returns auth_user_id1 e.g.
-    channels_create_v1('auth_user_id1', 'channel1', True) #returns channel_id1 e.g.
+    data['users'] = {}
+    data['channels'] = {}
+    a_u_id1 = auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1') #returns auth_user_id1 e.g.
+    ch_id = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #returns channel_id1 e.g.
     with pytest.raises(InputError):
-        channel_invite_v1('auth_user_id1', 'channel_id1', 'auth_user_id2')
+        channel_invite_v1(a_u_id1['auth_user_id'], ch_id['channel_id'], 1216374684571)
 
 # Channel_invite executed by user not in given channel
 def test_unauthorized_user():
-    data.clear()
-    auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1') #returns auth_user_id1 e.g.
-    auth_register_v1('example2@hotmail.com', 'password2', 'first_name2', 'last_name2') #returns auth_user_id2 e.g.
-    auth_register_v1('example3@hotmail.com', 'password3', 'first_name3', 'last_name3') #returns auth_user_id3 e.g.
-    channels_create_v1('auth_user_id1', 'channel1', True) #returns channel_id1 e.g.
+    data['users'] = {}
+    data['channels'] = {}
+    a_u_id1 = auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1') #returns auth_user_id1 e.g.
+    a_u_id2 = auth_register_v1('example2@hotmail.com', 'password2', 'first_name2', 'last_name2') #returns auth_user_id2 e.g.
+    a_u_id3 = auth_register_v1('example3@hotmail.com', 'password3', 'first_name3', 'last_name3') #returns auth_user_id3 e.g.
+    ch_id = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #returns channel_id1 e.g.
     with pytest.raises(AccessError):
-        channel_invite_v1('auth_user_id2', 'channel_id1', 'auth_user_id3')
+        channel_invite_v1(a_u_id2['auth_user_id'], ch_id['channel_id'], a_u_id3['auth_user_id'])
         
