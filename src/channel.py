6b5d16dc33e725b9bb704adf050df41d83167e38
Brendan Ye <z5254583@ad.unsw.edu.au>
2021-03-06 22:33:29 +1100

    Made the code neater and more pythonic

--- src/channel.py
+++ src/channel.py
@@ -3,56 +3,6 @@ from src.error import AccessError, InputError
 #from data import data, retrieve_data
 #from error import AccessError, InputError
 
-
-###############################################################################
-#                               HELPER FUNCTIONS                              #
-###############################################################################
-
-
-# Helper function to determine if a channel is a valid channel
-def is_valid_channel_id(channel_id):
-    data = retrieve_data()
-    for channel in data['channels']:
-        if channel_id == channel:
-            return True
-    return False
-
-
-# Helper function to determine if an authorised user is a member of the channel
-def is_valid_user_in_channel(user_id, channel_id):
-    # Get the channel from the given channel id
-    data = retrieve_data()
-    channel = data['channels'][channel_id]
-
-    for user in channel['all_members']:
-        if int(user_id) == user:
-            return True
-    
-    return False
-
-
-# Helper function to determine the number of messages in a given channel
-def num_messages(channel_id):
-    # Get the channel from the given channel id
-    data = retrieve_data()
-    channel = data['channels'][channel_id]
-
-    count = 0
-    for message in channel['messages']:
-        count += 1
-    
-    return count
-
-
-###############################################################################
-#                             END HELPER FUNCTIONS                            #
-###############################################################################
-
-
-###############################################################################
-#                               CHANNEL FUNCTIONS                             #
-###############################################################################
-
 # Invites a user (with user id u_id) to join a channel with ID channel_id
 # Once invited the user is added to the channel immediately
 def channel_invite_v1(auth_user_id, channel_id, u_id):
@@ -130,22 +80,27 @@ def channel_details_v1(auth_user_id, channel_id):
 
     return details_dict
 
+
+
+# Given a valid channel_id, return up to 50 messages in the channel
+# ASSUMPTION: Start IS NOT negative (originally I made it an input error but
+# apparently you're not allowed to raise any input or access errors other than
+# the ones listed in the spec, so its tests were removed altogether and was
+# replaced by an assumption)
 def channel_messages_v1(auth_user_id, channel_id, start):
+    data = retrieve_data()
     # Check to see if the given channel_id is a valid channel
-    if not is_valid_channel_id(channel_id):
+    if not any(channel == channel_id for channel in data['channels']):
         raise InputError("Channel id is not valid")
     # Check to see if the given user is actully in the given channel
-    elif not is_valid_user_in_channel(auth_user_id, channel_id):
+    elif not any(user == auth_user_id for user in data['channels'][channel_id]['all_members']):
         raise AccessError("The user is not in the channel")
     
     # Check to see if the given start value is larger than the number of
     # messages in the given channel
-    if start > num_messages(channel_id):
+    num_messages = len(data['channels'][channel_id]['messages'])
+    if start > num_messages:
         raise InputError("Inputted starting index is larger than the current number of messages in the channel")
-    
-    # Check to see if the given start is negative
-    if start < 0:
-        raise InputError("Inputted starting index is negative")
 
     # Initialise our message dictionary which we will be returning
     messages_dict = {
@@ -155,14 +110,13 @@ def channel_messages_v1(auth_user_id, channel_id, start):
     }
 
     # Get our current channel
-    data = retrieve_data()
     channel = data['channels'][channel_id]
     # ASSUMPTION: messages are APPENDED to our message list within the channel
     # key of our data dictionary
     # Reverse the order of the channel messages so the most recent message
     # appears in index 0 and the least recent in the last index
     messages_list = channel['messages'][::-1]
-    #messages_list = channel['messages']
+
 
     # Loop through our list and return up to 50 of the most recent messages
     # starting our index with the given start
@@ -181,11 +135,10 @@ def channel_messages_v1(auth_user_id, channel_id, start):
         messages_dict['end'] = -1
     # If the number of messages in the channel minus the given start divided
     # by 50 returns 1, this mean the most recent message has been returned
-    elif (num_messages(channel_id) - start) / 50 == 1:
+    elif (num_messages - start) / 50 == 1:
         messages_dict['end'] = -1
     else:
         messages_dict['end'] = start + 50
-
 
     return messages_dict
 
