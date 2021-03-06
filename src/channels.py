9268572eee4759051a155ba76c20577fd3eebb07
Nikki <z5260192@ad.unsw.edu.au>
2021-03-06 23:09:53 +1100

    Working code

--- src/channels.py
+++ src/channels.py
@@ -23,29 +23,26 @@ def channels_listall_v1(auth_user_id):
         ],
     }
 
+# Creates a new channel with that name that is either a public or private channel
 def channels_create_v1(auth_user_id, name, is_public):
 
     data = retrieve_data()
 
-    # error when creating a channel name longer than 20 characters
-    if len(name) > 20:
-        raise InputError("Channel name cannot be longer than 20 characters")
+    # InputError occurs when creating a channel name longer than 20 characters
+    if len(name) > 20: raise InputError("Channel name cannot be longer than 20 characters")
 
-    # AccessError occurs when input isauth_user_id
-    curr_user = {}
-    for user in data['users']:
-        if user == auth_user_id:
-            curr_user = user
-    if curr_user == {}:
-        raise AccessError("Invalid auth_user_id")
+    # AccessError occurs when input is invalid auth_user_id
+    if auth_user_id not in data['users']: raise AccessError("Invalid auth_user_id")
 
+    # Generate unique channel_id
     channel_id = int(uuid.uuid1())
 
+    # Add new channel to channels data
     data['channels'][channel_id] = {
         'name' : name, 
         'is_public' : is_public, 
-        'owner_members' : auth_user_id,
-        'all_members' : auth_user_id,
+        'owner_members': [auth_user_id],
+        'all_members': [auth_user_id],
         'messages' : [],
     }   
 
@@ -53,16 +50,4 @@ def channels_create_v1(auth_user_id, name, is_public):
         'channel_id': channel_id
     }
 
-'''
-# assert channel id is an integer
-if __name__ == '__main__' :
 
-    global data
-    
-    user1 = auth_register_v1('user1@email.com', 'User1_pass!', 'user1_first', 'user1_last')
-    print(user1)
-
-    print(     ) 
-    channel_id1 = channels_create_v1(user1['auth_user_id'], "Public Channel", True)
-    print(channel_id1)
-'''
