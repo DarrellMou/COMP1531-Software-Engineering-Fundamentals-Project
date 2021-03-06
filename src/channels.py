b3888c88be0c8e88f908504daa4c9570b3731466
KelNoMel <68493100+KelNoMel@users.noreply.github.com>
2021-03-06 23:04:28 +1100

    Stable Push
    All files should be of acceptible quality for this iteration, will do further work to improve style and add tests

--- src/channels.py
+++ src/channels.py
@@ -1,26 +1,42 @@
+from src.data import retrieve_data
+import uuid
+
+from src.error import InputError
+from src.error import AccessError
+
 def channels_list_v1(auth_user_id):
     data = retrieve_data()
 
     # No parameter errors
     # List of channels
     channel_ids = data['channels']
+    channel_list = []
 
     # Search through individual channels for specific user
     for channel in channel_ids:
-        # Create a list of channel attributes
-        name = data['channels'][channel]['name']
-        owners = data['channels'][channel]['owners']
-        members = data['channels'][channel]['members']
-
-        #Look for positive match
-        for owner in owners:
-            if owner == auth_user_id:
-                print('channel_id: ' + channel)
-                print('\nname: ' + name)
-        for member in members:
+        for member in data['channels'][channel]['all_members']:
             if member == auth_user_id:
-                print('channel_id: ' + channel)
-                print('\nname: ' + name)
+                # Create a list of channel attributes
+                channel_details = {
+                    'channel_id' : channel,
+                    'name' : data['channels'][channel]['name'],
+                }
+                channel_list.append(channel_details)
+    
+    return{
+        'channels': channel_list
+    }
+
+
+
+    #Look for positive match, if found print channel details
+    for member in data['channels']:
+        if member == auth_user_id:
+            return {
+                'channels': channel_list
+            }
+
+
 
 
 def channels_listall_v1(auth_user_id):
@@ -34,6 +50,31 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
+
+    data = retrieve_data()
+
+    # error when creating a channel name longer than 20 characters
+    if len(name) > 20:
+        raise InputError("Channel name cannot be longer than 20 characters")
+
+    # AccessError occurs when input is invalid auth_user_id
+    curr_user = {}
+    for user in data['users']:
+        if user == auth_user_id:
+            curr_user = user
+    if curr_user == {}:
+        raise AccessError("Invalid auth_user_id")
+
+    channel_id = int(uuid.uuid1())
+
+    data['channels'][channel_id] = {
+        'name' : name, 
+        'is_public' : is_public, 
+        'owner_members': [auth_user_id],
+        'all_members': [auth_user_id],
+        'messages' : [],
+    }   
+
     return {
-        'channel_id': 1,
+        'channel_id': channel_id
     }
--- tests/channels_list_v1_tests.py
+++ tests/channels_list_v1_tests.py
@@ -1,5 +1,6 @@
 import pytest
-from src.data import data
+from src.data import reset_data
+from src.data import retrieve_data
 
 from src.error import InputError
 from src.error import AccessError
@@ -8,63 +9,76 @@ from src.auth import auth_register_v1
 from src.channel import channel_join_v1
 from src.channel import channel_details_v1
 from src.channels import channels_create_v1
+from src.channels import channels_list_v1
 
 #Cases start here
 
 #Standard Case, only one channel joined
 def test_standard():
-    data.clear()
-    auth-auth_register_v1('temp1@gmail.com','pw1','first1','last1') #auth_user_id1 created
-    auth-auth_register_v1('temp2@gmail.com','pw2','first2','last2') #auth_user_id2 created
-    channels_create_v1('auth_user_id1', 'channel1', True) #Public channel created
-    channel_join_v1('auth_user_id2', 'channel_id1')
+    reset_data()
+    a_u_id1 = auth_register_v1('temp1@gmail.com','password1','first1','last1') #auth_user_id1 created
+    a_u_id2 = auth_register_v1('temp2@gmail.com','password2','first2','last2') #auth_user_id2 created
+    chid1 = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #Public channel created
+    chid2 = channels_create_v1(a_u_id2['auth_user_id'], 'channel2', True) #Public channel created
+    chid3 = channels_create_v1(a_u_id2['auth_user_id'], 'channel3', True) #Public channel created
+    #channel_join_v1('auth_user_id2', 'channel_id1')
     
-    assert channels_list_v1('auth_user_id2') == {
+    assert channels_list_v1(a_u_id2['auth_user_id']) == {
         'channels': [
             {
-                'channel_id': 1,
-                'name': 'channel1',
+                'channel_id': chid2['channel_id'],
+                'name': 'channel2',
+            },
+            {
+                'channel_id': chid3['channel_id'],
+                'name': 'channel3',
             },
         ],
     }
 #Case where a user joins multiple channels
 def test_multiple_channels():
-    data.clear()
-    auth-auth_register_v1('temp1@gmail.com','pw1','first1','last1') #auth_user_id1 created
-    auth-auth_register_v1('temp2@gmail.com','pw2','first2','last2') #auth_user_id2 created
-    channels_create_v1('auth_user_id1', 'channel1', True) #Public channel1 created
-    channels_create_v1('auth_user_id1', 'channel2', True) #Public channel2 created
-    channels_create_v1('auth_user_id1', 'channel3', True) #Public channel3 created
-    channel_join_v1('auth_user_id2', 'channel_id1')
-    channel_join_v1('auth_user_id2', 'channel_id2')
-    channel_join_v1('auth_user_id2', 'channel_id3')
+    reset_data()
+    a_u_id1 = auth_register_v1('temp1@gmail.com','password1','first1','last1') #auth_user_id1 created
+    a_u_id2 = auth_register_v1('temp2@gmail.com','password2','first2','last2') #auth_user_id2 created
+    chid1 = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #Public channel1 created
+    chid2 = channels_create_v1(a_u_id1['auth_user_id'], 'channel2', True) #Public channel2 created
+    chid3 = channels_create_v1(a_u_id1['auth_user_id'], 'channel3', True) #Public channel3 created
+    chid4 = channels_create_v1(a_u_id2['auth_user_id'], 'channel4', True) #Public channel3 created
+    chid5 = channels_create_v1(a_u_id2['auth_user_id'], 'channel5', True) #Public channel3 created
+    chid6 = channels_create_v1(a_u_id2['auth_user_id'], 'channel6', True) #Public channel3 created
+    chid7 = channels_create_v1(a_u_id2['auth_user_id'], 'channel7', True) #Public channel3 created
+    #channel_join_v1('auth_user_id2', 'channel_id1')
+    #channel_join_v1('auth_user_id2', 'channel_id2')
+    #channel_join_v1('auth_user_id2', 'channel_id3')
 
-    assert channels_list_v1('auth_user_id2') == {
+    assert channels_list_v1(a_u_id2['auth_user_id']) == {
         'channels': [
             {
-                'channel_id': 1,
-                'name': 'channel1',
+                'channel_id': chid4['channel_id'],
+                'name': 'channel4',
             },
             {
-                'channel_id': 2,
-                'name': 'channel2',
+                'channel_id': chid5['channel_id'],
+                'name': 'channel5',
             },
             {
-                'channel_id': 3,
-                'name': 'channel3',
+                'channel_id': chid6['channel_id'],
+                'name': 'channel6',
+            },
+            {
+                'channel_id': chid7['channel_id'],
+                'name': 'channel7',
             },
         ],
     }
 
 #Case where user is valid, but is a member of no channels
 def test_memberless():
-    data.clear()
-    auth-auth_register_v1('temp1@gmail.com','pw1','first1','last1') #auth_user_id1 created
+    reset_data()
+    a_u_id1 = auth_register_v1('temp1@gmail.com','password1','first1','last1') #auth_user_id1 created
 
-    assert channels_list_v1('auth_user_id1') == {
-        'channels': [
-
-        ],
+    assert channels_list_v1(a_u_id1['auth_user_id']) == {
+        'channels': [],
     }
 
 #Assumptions, users don't have to be in a channel
