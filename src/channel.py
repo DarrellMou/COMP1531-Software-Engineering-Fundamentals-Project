ef65d86ed78058ee1df536d944a77d54b0a64b69
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-03-07 01:33:28 +1100

    Added channel_invite_v1 and tests

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,30 @@
+from src.data import data, retrieve_data
+from src.error import AccessError, InputError
+#from data import data, retrieve_data
+#from error import AccessError, InputError
+
+# Invites a user (with user id u_id) to join a channel with ID channel_id
+# Once invited the user is added to the channel immediately
 def channel_invite_v1(auth_user_id, channel_id, u_id):
-    return {
-    }
+    data = retrieve_data()
+
+    # Checks if given channel_id is valid
+    if not any(channel == channel_id for channel in data['channels']): raise InputError
+
+    # Checks if user exists
+    if not any(user == u_id for user in data['users']): raise InputError
+
+    # Checks if the auth_user is in channel
+    if not any(user == auth_user_id for user in data['channels'][channel_id]['all_members']): raise AccessError
+
+    # Appends new user to given channel
+    # Assume no duplicate entries allowed
+    # Assume no inviting themselves
+    # Assume inviting people outside channel only
+    if not any(user == u_id for user in data['channels'][channel_id]['all_members']):
+        data['channels'][channel_id]['all_members'].append(u_id)
+
+    return {}
 
 def channel_details_v1(auth_user_id, channel_id):
     return {
