24da2b41fde54ab5d12c3dca8cc89038d463a8d9
Nikki <z5260192@ad.unsw.edu.au>
2021-03-06 23:10:56 +1100

    Updated data set

--- src/data.py
+++ src/data.py
@@ -2,7 +2,6 @@
 data = {
     'users' : {
         35746842521 : {
-            'u_id' : 1,
             'name_first' : 'first_name1',
             'name_last' : 'last_name1',
             'email' : 'example1@hotmail.com',
@@ -10,7 +9,6 @@ data = {
             'handle_str' : 'first_name1last_name',
         },
         11753764853 : {
-            'u_id' : 2,
             'name_first' : 'first_name2',
             'name_last' : 'last_name2',
             'email' : 'example2@hotmail.com',
@@ -25,48 +23,56 @@ data = {
             'owner_members' : ['auth_user_id1'],
             'all_members' : ['auth_user_id1', 'auth_user_id2'],
             'messages' : [
-                {'message_id1' : {
-                    'auth_user_id' : 35746842521,
-                    'message' : 'Hello World1',
-                    'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)',
-                    },
+                {
+                    'message_id': 4561328123,
+                    'u_id': 35746842521,
+                    'message': 'Hello World1',
+                    'time_created': 123416589,
                 },
-                {'message_id2' : {
-                    'auth_user_id' : 11753764853,
-                    'message' : 'Hello World2',
-                    'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)',
-                    },
+                {
+                    'message_id': 61510648893,
+                    'u_id': 11753764853,
+                    'message': 'Hello World2',
+                    'time_created': 123456789,
                 },
-            ],
+            ]
         },
         31627643273 : {
             'name' : 'channel2',
             'is_public' : True,
-            'owner_members' : ['auth_user_id1'],
-            'all_members' : ['auth_user_id1', 'auth_user_id2'],
+            'owner_members' : [123456789],
+            'all_members' : [12356789, 12389473129847],
             'messages' : [
-                {'message_id1' : {
-                    'auth_user_id' : 35746842521,
-                    'message' : 'Hello World1',
-                    'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)',
-                    },
+                {
+                    'message_id': 12354122383,
+                    'u_id': 35746842521,
+                    'message': 'Hello World1',
+                    'time_created': 45132806512,
                 },
-                {'message_id2' : {
-                    'auth_user_id' : 11753764853,
-                    'message' : 'Hello World2',
-                    'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)',
-                    },
-                },
+                {
+                    'message_id': 123156231064,
+                    'u_id': 11753764853,
+                    'message': 'Hello World2',
+                    'time_created': 68741450315603,
+                }
             ],
         },
     },
 }
 
+
+# Function to reset the data to default (assists in testing)
+def reset_data():
+    global data
+    data = {
+        "users" : {},
+        "channels" : {}
+    }
+    return data
+
 def retrieve_data():
     global data
     return data
-
-
 
 
 
