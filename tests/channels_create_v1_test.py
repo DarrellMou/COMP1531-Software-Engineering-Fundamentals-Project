187e324514dd70213efeabf63e990bccbc7b71fa
Nikki <z5260192@ad.unsw.edu.au>
2021-03-06 22:20:43 +1100

    Added working tests

--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.auth import auth_register_v1
 from src.channel import channel_join_v1
-from src.channels import channels_create_v1
+from src.channels import channels_create_v1, channels_listall_v1
 from src.error import InputError, AccessError
 from src.data import reset_data
 
@@ -23,11 +23,13 @@ def setup_user():
         'user5' : a_u_id5
     }
 
+
 # error when creating a channel with an invalid auth_user_id
 def test_channels_create_access_error():
 
     with pytest.raises(AccessError):
         channels_create_v1("invalid a_u_id", "Channel", True)
+
 
 # error when creating a channel name longer than 20 characters
 def test_channels_create_input_error():
@@ -42,6 +44,7 @@ def test_channels_create_input_error():
     with pytest.raises(InputError):
         channels_create_v1(users['user1']['auth_user_id'], "This is longer than 20", False)
 
+
 # assert channel id is an integer
 def test_channels_create_return_value():
     
@@ -49,6 +52,39 @@ def test_channels_create_return_value():
 
     channel_id1 = channels_create_v1(users['user1']['auth_user_id'], "Public Channel", True)
     assert isinstance(channel_id1['channel_id'], int)
+
+
+# create channels of the same name
+def test_channels_create_same_name():
+
+    users = setup_user()
+
+    channel_id2 = channels_create_v1(users['user1']['auth_user_id'], "Public Channel", True)
+    channel_id3 = channels_create_v1(users['user1']['auth_user_id'], "Public Channel", True)
+
+    # ensure channels_listall returns correct values
+    channel_list = channels_listall_v1(users['user3']['auth_user_id'])
+
+    assert channel_list['channels'][0]['channel_id'] == channel_id2['channel_id']
+    assert channel_list['channels'][0]['name'] == 'Public Channel'
+
+    assert channel_list['channels'][1]['channel_id'] == channel_id3['channel_id']
+    assert channel_list['channels'][1]['name'] == 'Public Channel'
+
+
+# create channel with no name 
+def test_channels_create_no_name():
+
+    users = setup_user()
+
+    channel_id4 = channels_create_v1(users['user1']['auth_user_id'], "", True)
+
+    # ensure channels_listall returns correct values
+    channel_list = channels_listall_v1(users['user3']['auth_user_id'])
+
+    assert channel_list['channels'][0]['channel_id'] == channel_id4['channel_id']
+    assert channel_list['channels'][0]['name'] == ''
+
 
 '''
 # test private channel 
