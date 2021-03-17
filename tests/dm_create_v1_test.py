6c5049069fe959e6f5900e6fdc5d445794967c05
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-03-17 23:02:46 +1100

    Fixed tests

--- tests/dm_create_v1_test.py
+++ tests/dm_create_v1_test.py
@@ -1,35 +1,32 @@
 import pytest
 
-from src.data import reset_data
+from src.data import reset_data, data
 
 from src.error import InputError
 from src.error import AccessError
 
-from src.auth import auth_register_v1
-from src.auth import auth_decode_token
+from src.auth import auth_register_v1, auth_decode_token
 from src.dm import dm_create_v1
 
-@pytest.fixture(autouse=True)
-def reset():
-    reset_data()
-
 def test_function():
+    data = reset_data()
     a_u_id1 = auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1')
     a_u_id2 = auth_register_v1('example2@hotmail.com', 'password2', 'first_name2', 'last_name2')
 
-    assert dm_create_v1(auth_decode_token(a_u_id1['token']), [a_u_id1['auth_user_id'], a_u_id2['auth_user_id']]) == {
-        'dm_id': data[a_u_id1['auth_user_id']]['dms'[0]],
+    assert dm_create_v1(a_u_id1['token'], [a_u_id1['auth_user_id'], a_u_id2['auth_user_id']]) == {
+        'dm_id': data['users'][a_u_id1['auth_user_id']]['dms'][0],
         'dm_name': 'first_name1last_name, first_name2last_name'
     }
 
 def test_multiple():
+    data = reset_data()
     a_u_id1 = auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1')
     a_u_id2 = auth_register_v1('example2@hotmail.com', 'password2', 'first_name2', 'last_name2')
     a_u_id3 = auth_register_v1('example3@hotmail.com', 'password3', 'first_name3', 'last_name3')
     a_u_id4 = auth_register_v1('example4@hotmail.com', 'password4', 'first_name4', 'last_name4')
     a_u_id5 = auth_register_v1('example5@hotmail.com', 'password5', 'first_name5', 'last_name5')
-    assert dm_create_v1(auth_decode_token(a_u_id1['token']), [a_u_id1['auth_user_id'], a_u_id2['auth_user_id'], a_u_id3['auth_user_id'], a_u_id4['auth_user_id'], a_u_id5['auth_user_id']]) == {
-        'dm_id': data[a_u_id1['auth_user_id']]['dms'[0]],
+    assert dm_create_v1(a_u_id1['token'], [a_u_id1['auth_user_id'], a_u_id2['auth_user_id'], a_u_id3['auth_user_id'], a_u_id4['auth_user_id'], a_u_id5['auth_user_id']]) == {
+        'dm_id': data['users'][a_u_id1['auth_user_id']]['dms'][0],
         'dm_name': 'first_name1last_name, first_name2last_name, first_name3last_name, first_name4last_name, first_name5last_name'
     }
 
@@ -37,5 +34,5 @@ def test_multiple():
 
 # dm_create given invalid user
 def test_invalid_user():
-    with pytest.raises(InputError):
+    with pytest.raises(AccessError):
         dm_create_v1(12345, [12345, 67890])
