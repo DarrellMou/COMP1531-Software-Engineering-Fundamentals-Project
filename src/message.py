ebed76d5eb7a39ce67ee1c79ef12cce3d6bb10ef
Nikki <z5260192@ad.unsw.edu.au>
2021-03-18 15:40:57 +1100

    Written edge case code for message.py and changed token input code in channels (wrong branch)

--- src/message.py
+++ src/message.py
@@ -12,5 +12,21 @@ def message_edit_v1(auth_user_id, message_id, message):
     }
 
 def message_senddm_v1(token, dm_id, message):
+
+    data = retrieve_data()
+
+    # Checks if dm_id is valid
+    if dm_id not in data['dms']: raise InputError
+
+    # Checks if token exists
+    if not auth_token_ok(token): raise AccessError
+    auth_user_id = auth_decode_token(token)
+
+    # Checks if user belongs in dm
+    if auth_user_id not in data['dms'][dm_id]['members']: raise AccessError
+
+    # Checks if message is no more than 1000 characters
+    if count(message) > 1000: raise InputError
+
     return {
     }
