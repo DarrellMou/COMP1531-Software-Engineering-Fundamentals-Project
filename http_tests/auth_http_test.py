b799f6845ad20824157020e458e284164ff285eb
winston <winstonlin@nsa.gov>
2021-03-19 18:24:30 +1100

    fix pylint and errors in auth_http_test

--- http_tests/auth_http_test.py
+++ http_tests/auth_http_test.py
@@ -11,8 +11,8 @@ def test_auth_register_api_valid(client):
 	assert response.status_code == 201
 	assert response.content_type == 'application/json'
 
-	assert json_data['token'] != ''
-	assert json_data['auth_user_id'] != -1
+	assert json_data['token'] and json_data['token'] != ''
+	assert json_data['auth_user_id'] and json_data['auth_user_id'] != -1
 
 
 def test_auth_register_api_invalid_request(client):
@@ -31,13 +31,13 @@ def test_auth_login_api_valid(client):
 	assert response_register.status_code == 201
 	assert response_register.content_type == 'application/json'
 
-	response_login = client.post('/login', json={'email':'12382193@outlook.com', 'password':'123123kjdfd'})
+	response_login = client.post('/login', json={'email':'1238293@outlook.com', 'password':'123123kjdfd'})
 	assert response_login.content_type == 'application/json'
 	assert response_login.status_code == 201
 
 	json_data_login = response_login.get_json()
 	assert json_data_login['token']
-	assert json_data_login['auth_user_id']
+	assert json_data_login['auth_user_id'] == json_data_register['auth_user_id']
 
 
 def test_auth_login_api_invalid(client):
