6dfdc5d964781e70df117e0a254ab02bfe214a45
winston <winstonlin@nsa.gov>
2021-03-19 13:31:40 +1100

    forgot to add __init__.py in last commit

--- tests/base_http_test.py
+++ tests/base_http_test.py
@@ -4,13 +4,13 @@ from src import create_app
 
 # a new app instance for every test
 @pytest.fixture
-def app():
+def app_for_testing():
 	a = create_app()
 
 	yield a # a is a generator 
 
 
 @pytest.fixture 
-def client(app):
-	return app.test_client()
+def client(app_for_testing):
+	return app_for_testing.test_client()
 
