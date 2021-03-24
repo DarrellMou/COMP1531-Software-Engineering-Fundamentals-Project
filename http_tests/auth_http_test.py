e030a49615cf9ae622a3afd6ab82d9361a28144b
winston lin <winstonlin@nsa.gov>
2021-03-25 00:16:17 +1100

    finished tests for auth_logout

--- http_tests/auth_http_test.py
+++ http_tests/auth_http_test.py
@@ -2,6 +2,8 @@ from src.server import APP
 from src.error import InputError
 from http_tests import *
 import requests
+import time
+from src.auth import blacklist, auth_decode_token, auth_token_ok
 
 # client and app are pytest fixtures
 def test_auth_register_api_valid(client):
@@ -60,13 +62,43 @@ def test_auth_login_api_invalid(client):
 	assert json_data_login['auth_user_id']
 
 def test_auth_logout_api(client):
-	response_register = client.post('/register', json={'email':'1238293@outlook.com', 'password':'123123kjdfd', 'first_name':'winston', 'last_name':'lin'})
+	# register
+	response_register = client.post('/register', json={'email':'testing123@gmail.com', 'password':'1234567ABC', 'first_name':'winston', 'last_name':'lin'})
 	json_data_register = response_register.get_json() # or just json
+	assert response_register.content_type == 'application/json'
+	assert response_register.status_code == 201
 	token_kept_by_client = json_data_register['token']
 
+	# logout
 	response_logout = client.post('/logout', json={'token':token_kept_by_client})
+	assert response_logout.status_code == 201
 	assert response_logout.json['is_success'] == True
 
-	# logout again with the same token, it is an invalid action since we've already logged out
+	# logout again with the same token, blacklisted since we've already logged out
 	response_logout2 = client.post('/logout', json={'token':token_kept_by_client})
-	assert response_logout.json['is_success'] == False
+	assert response_logout2.json['is_success'] == False
+	assert response_logout2.status_code == 408
+
+
+def test_auth_logout_api_logging_back(client):
+	# register
+	response_register = client.post('/register', json={'email':'testing123@gmail.com', 'password':'1234567ABC', 'first_name':'winston', 'last_name':'lin'})
+	json_data_register = response_register.get_json() # or just json
+	assert response_register.content_type == 'application/json'
+	assert response_register.status_code == 201
+	token_kept_by_client = json_data_register['token']
+	auth_user_id = auth_decode_token(token_kept_by_client)
+
+	# logout
+	response_logout = client.post('/logout', json={'token':token_kept_by_client})
+	assert response_logout.status_code == 201
+	assert response_logout.json['is_success'] == True
+
+	# log back in
+	response_login = client.post('/login', json={'email':'testing123@gmail.com', 'password':'1234567ABC'})
+	assert response_login.content_type == 'application/json'
+	assert response_login.status_code == 201
+
+	# assert the user is no longer in the blacklist and token is valid again
+	assert auth_user_id not in blacklist
+	assert auth_token_ok(token_kept_by_client) == True
