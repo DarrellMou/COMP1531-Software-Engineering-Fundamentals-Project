d3e02f76c3ea59822de7d6bd4987b342bc2b230f
KelNoMel <68493100+KelNoMel@users.noreply.github.com>
2021-03-26 16:34:37 +1100

    channel_join_v2 tests work now

--- tests/channel_join_v2_test.py
+++ tests/channel_join_v2_test.py
@@ -3,7 +3,7 @@ from src.data import reset_data, retrieve_data
 from src.error import InputError, AccessError
 
 from src.auth import auth_register_v1
-from src.channel import channel_join_v2, channel_details_v2
+from src.channel import channel_join_v2, channel_details_v1
 from src.channels import channels_create_v1, channels_list_v2
 
 #Cases start here
@@ -13,11 +13,11 @@ def test_standard():
     reset_data()
     a_u_id1 = auth_register_v1('temp1@gmail.com','password1','first1','last1') #auth_user_id1 created
     a_u_id2 = auth_register_v1('temp2@gmail.com','password2','first2','last2') #auth_user_id2 created
-    chid1 = channels_create_v1(users[a_u_id1]['token'], 'channel1', True) #Public channel created
-    channel_join_v2(users[a_u_id2]['token'], chid1['channel_id']) #User 2 joins channel 1 as regular member
+    chid1 = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #Public channel created
+    channel_join_v2(a_u_id2['token'], chid1['channel_id']) #User 2 joins channel 1 as regular member
     
     # Expect a list containing channel 1
-    assert channels_list_v2([a_u_id2]['token']) == {
+    assert channels_list_v2(a_u_id2['token']) == {
         'channels': [
             {
                 'channel_id': chid1['channel_id'],
@@ -31,17 +31,17 @@ def test_multiple_channels_joined():
     reset_data()
     a_u_id1 = auth_register_v1('temp1@gmail.com','password1','first1','last1') #auth_user_id1 created
     a_u_id2 = auth_register_v1('temp2@gmail.com','password2','first2','last2') #auth_user_id2 created
-    chid1 = channels_create_v1(users[a_u_id1]['token'], 'channel1', True) #Public channel1 created
-    chid2 = channels_create_v1(users[a_u_id1]['token'], 'channel2', True) #Public channel2 created
-    chid3 = channels_create_v1(users[a_u_id1]['token'], 'channel3', True) #Public channel3 created
-    chid4 = channels_create_v1(users[a_u_id1]['token'], 'channel4', True) #Public channel4 created
-    channel_join_v2(users[a_u_id2]['token'], chid2['channel_id']) #User 2 joins channel 2 as regular member
-    channel_join_v2(users[a_u_id2]['token'], chid3['channel_id']) #User 2 joins channel 3 as regular member
-    channel_join_v2(users[a_u_id2]['token'], chid4['channel_id']) #User 2 joins channel 4 as regular member
+    chid1 = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #Public channel1 created
+    chid2 = channels_create_v1(a_u_id1['auth_user_id'], 'channel2', True) #Public channel2 created
+    chid3 = channels_create_v1(a_u_id1['auth_user_id'], 'channel3', True) #Public channel3 created
+    chid4 = channels_create_v1(a_u_id1['auth_user_id'], 'channel4', True) #Public channel4 created
+    channel_join_v2(a_u_id2['token'], chid2['channel_id']) #User 2 joins channel 2 as regular member
+    channel_join_v2(a_u_id2['token'], chid3['channel_id']) #User 2 joins channel 3 as regular member
+    channel_join_v2(a_u_id2['token'], chid4['channel_id']) #User 2 joins channel 4 as regular member
     
 
     # Expecting a list containing channels 2-4
-    assert channels_list_v2([a_u_id2]['token']) == {
+    assert channels_list_v2(a_u_id2['token']) == {
         'channels': [
             {
                 'channel_id': chid2['channel_id'],
@@ -58,20 +58,20 @@ def test_multiple_channels_joined():
         ],
     }
 
-#Case where multiple users join one channel
+#Case where multiple  join one channel
 def test_multiple_joiners():
     reset_data()
     a_u_id1 = auth_register_v1('temp1@gmail.com','password1','first1','last1') #auth_user_id1 created
     a_u_id2 = auth_register_v1('temp2@gmail.com','password2','first2','last2') #auth_user_id2 created
     a_u_id3 = auth_register_v1('temp3@gmail.com','password3','first3','last3') #auth_user_id3 created
     a_u_id4 = auth_register_v1('temp4@gmail.com','password4','first4','last4') #auth_user_id4 created
-    chid1 = channels_create_v1(users[a_u_id1]['token'], 'channel1', True) #Public channel1 created
-    channel_join_v2(users[a_u_id2]['token'], chid1['channel_id']) #User 2 joins channel 1 as regular member
-    channel_join_v2(users[a_u_id3]['token'], chid1['channel_id']) #User 3 joins channel 1 as regular member
-    channel_join_v2(users[a_u_id4]['token'], chid1['channel_id']) #User 4 joins channel 1 as regular member
+    chid1 = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #Public channel1 created
+    channel_join_v2(a_u_id2['token'], chid1['channel_id']) #User 2 joins channel 1 as regular member
+    channel_join_v2(a_u_id3['token'], chid1['channel_id']) #User 3 joins channel 1 as regular member
+    channel_join_v2(a_u_id4['token'], chid1['channel_id']) #User 4 joins channel 1 as regular member
 
     # Expecting a owner members including 1, and all members including 1-4
-    assert channel_details_v2([a_u_id1]['token'], chid1['channel_id']) == {
+    assert channel_details_v1(a_u_id1['auth_user_id'], chid1['channel_id']) == {
         'name': 'channel1',
         'owner_members': [
             {
@@ -109,19 +109,19 @@ def test_private_channel():
     reset_data()
     a_u_id1 = auth_register_v1('temp1@gmail.com','password1','first1','last1') #auth_user_id1 created
     a_u_id2 = auth_register_v1('temp2@gmail.com','password2','first2','last2') #auth_user_id2 created
-    chid1 = channels_create_v1(users[a_u_id1]['token'], 'channel1', False) #Private channel created
+    chid1 = channels_create_v1(a_u_id1['auth_user_id'], 'channel1', False) #Private channel created
 
     with pytest.raises(AccessError):
-        channel_join_v2(users[a_u_id2]['token'], chid1['channel_id']) #Channel_id1 is a private channel
+        channel_join_v2(a_u_id2['token'], chid1['channel_id']) #Channel_id1 is a private channel
 
 #Case where channel_join is given the id of a non-existent channel
 def test_invalid_channel():
     reset_data()
     a_u_id1 = auth_register_v1('temp1@gmail.com','password1','first1','last1') #auth_user_id1 created
     a_u_id2 = auth_register_v1('temp2@gmail.com','password2','first2','last2') #auth_user_id2 created
-    channels_create_v1(users[a_u_id1]['token'], 'channel1', True) #Public channel created
+    channels_create_v1(a_u_id1['auth_user_id'], 'channel1', True) #Public channel created
 
     with pytest.raises(InputError):
-        channel_join_v2(users[a_u_id2]['token'], 123) #Channel_id2 doesn't exist
+        channel_join_v2(a_u_id2['token'], 123) #Channel_id2 doesn't exist
 
-#Assumptions, users who are already in a channel will not join it again
+#Assumptions,  who are already in a channel will not join it again
