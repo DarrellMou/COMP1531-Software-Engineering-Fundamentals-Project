017a9802e4fd062f08d5cda8416b08440c654a56
KelNoMel <68493100+KelNoMel@users.noreply.github.com>
2021-03-27 23:18:41 +1100

    Data Structure and Get notifciations function coded
    This format should be final, now need to work on the functions that create the notifications

--- src/data.py
+++ src/data.py
@@ -14,12 +14,12 @@ data = {
                 {
                     'channel_id': 14723573315,
                     'dm_id': -1,
-                    'notification_message': 'Hello World1',
+                    'notification_message': 'first_name2last_name added you to channel1',
                 },
                 {
                     'channel_id': -1,
                     'dm_id': 4561328123,
-                    'notification_message': 'Hello World2',
+                    'notification_message': 'first_name2last_name reacted to your message in channel1',
                 },
             ]
         },
@@ -34,12 +34,12 @@ data = {
                 {
                     'channel_id': 14723573315,
                     'dm_id': -1,
-                    'notification_message': 'Hello World3',
+                    'notification_message': 'first_name1last_name added you to channel1',
                 },
                 {
                     'channel_id': -1,
                     'dm_id': 4561328124,
-                    'notification_message': 'Hello World4',
+                    'notification_message': 'first_name1last_name reacted to your message in channel1',
                 },
             ]
         },
---
+++ src/notifications.py
@@ -0,0 +1,7 @@
+from src.data import data, retrieve_data
+from src.auth import auth_decode_token
+
+def notifications_get_v1(token):
+    data = retrieve_data()
+    user_id = auth_decode_token(token)
+    return data['users'][user_id]['notifications'])
