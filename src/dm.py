dc2988ab2df03c1ff55e2db89c82a640610507d4
Brendan Ye <z5254583@ad.unsw.edu.au>
2021-03-27 22:00:42 +1100

    Created the dm_messages function based off of the channel_messages function

--- src/dm.py
+++ src/dm.py
@@ -158,3 +158,90 @@ def dm_leave_v1(token, dm_id):
     data['dms'][dm_id]['members'].remove(auth_user_id)
 
     return {}
+
+
+def dm_messages_v1(token, dm_id, start):
+    data = retrieve_data()
+
+    # Check to see if token is valid
+    if not auth_token_ok(token):
+        raise AccessError("The given token is not valid")
+
+    # Check to see if the given channel_id is a valid channel
+    if dm_id not in data['dms']:
+        raise InputError("dm_id is not valid")
+
+    # Check to see if the given user (token) is actully in the given channel
+    user_id = auth_decode_token(token)
+    if user_id not in data['dms'][dm_id]['members']:
+        raise AccessError("The user corresponding to the given token is not in the dm")
+
+    
+    # Check to see if the given start value is larger than the number of
+    # messages in the given channel
+    num_messages = len(data['dms'][dm_id]['messages'])
+    if start > num_messages:
+        raise InputError("Inputted starting index is larger than the current number of messages in the dm")
+
+    # Initialise our message dictionary which we will be returning
+    messages_dict = {
+        'messages': [],
+        'start': start,
+        'end': 0
+    }
+
+    # Get our current channel
+    dm = data['dms'][dm_id]
+    # ASSUMPTION: messages are APPENDED to our message list within the channel
+    # key of our data dictionary
+    # Reverse the order of the channel messages so the most recent message
+    # appears in index 0 and the least recent in the last index
+    messages_list = channel['messages'][::-1]
+
+
+    # Loop through our list and return up to 50 of the most recent messages
+    # starting our index with the given start
+    count = 0
+    for message in messages_list:
+        # Check to see if the message has been removed and if it has then
+        # skip it
+        if is_message_removed(message['message_id']):
+            continue
+        
+        # Starting off at the start index, add up to 50 messages to the list
+        # in the messages dictionary
+        if count >= start and count < (start + 50):
+            messages_dict['messages'].append(message)
+        count += 1
+
+    # If 50 messages were added, then the most recent message is going to be
+    # returned and as per the spec, 'end' should return -1. Otherwise, end
+    # should return (start + 50)
+    if len(messages_dict['messages']) != 50:
+        messages_dict['end'] = -1
+    # If the number of messages in the channel minus the given start divided
+    # by 50 returns 1, this mean the most recent message has been returned
+    elif (num_messages - start) / 50 == 1:
+        messages_dict['end'] = -1
+    else:
+        messages_dict['end'] = start + 50
+
+    return messages_dict
+
+
+
+
+###############################################################################
+#                               HELPER FUNCTIONS                              #
+###############################################################################
+
+# A function to check whether a message with given message_id is removed
+def is_message_removed(msg_id):
+    data = retrieve_data()
+    count = 0
+    while count < len(data['messages']):
+        if data['messages'][count]['message_id'] == msg_id:
+            if data['messages'][count]['is_removed']:
+                return True
+        count += 1
+    return False
