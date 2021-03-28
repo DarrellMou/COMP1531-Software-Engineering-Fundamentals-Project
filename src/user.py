5c6cd8e33f1022977afcb4ed4802a5c3bcdd2884
winston lin <winstonlin@nsa.gov>
2021-03-28 18:08:30 +1100

    implement user/profile/setemail/v2 and sethandle and users/all routes, all user profile routes are completed

--- src/user.py
+++ src/user.py
@@ -61,3 +61,77 @@ def user_profile_setname_v2():
         data['users'][auth_user_id]['name_first'] = name_first
 
     return make_response(jsonify({}))
+
+
+@bp.route('user/profile/setemail/v2', methods=['PUT'])
+def user_profile_setemail_v1():
+
+    data = retrieve_data()
+
+    token = request.json['token']
+    new_email = request.json['email']
+
+    if not auth_token_ok(token):
+        raise InputError('invalid token')
+
+    # email format check 
+    if not auth_email_format(new_email):
+        raise InputError('invalid email format')
+
+    # if email already used by another user 
+    if any(user['email'] == new_email for user in data['users'].values()):
+        raise InputError('email already exists') 
+
+    auth_user_id = auth_decode_token(token)
+
+    if auth_user_id in data['users']:
+        data['users'][auth_user_id]['email'] = new_email
+
+    return make_response(jsonify({}))
+
+
+@bp.route('user/profile/sethandle/v1', methods=['PUT'])
+def user_profile_sethandle_v1():
+    data = retrieve_data()
+
+    token = request.json['token']
+    new_handle = request.json['handle_str']
+
+    if not auth_token_ok(token):
+        raise InputError('invalid token')
+
+    # check handle format
+    if len(new_handle) not in range(3, 20):
+        raise InputError('invalid handle length')
+
+    # if email already used by another user 
+    if any(user['handle_str'] == new_handle for user in data['users'].values()):
+        raise InputError('handle already exists') 
+
+    auth_user_id = auth_decode_token(token)
+
+    if auth_user_id in data['users']:
+        data['users'][auth_user_id]['handle_str'] = new_handle
+
+    return make_response(jsonify({}))
+
+
+@bp.route('users/all/v1', methods=['GET'])
+def users_all_v1():
+    data = retrieve_data()
+
+    token = request.json['token']
+
+    if not auth_token_ok(token):
+        raise InputError('invalid token')
+
+    return make_response(jsonify(data['users']))
+
+    # both json.dumps() and jsonify encodes integer as string, 
+    # d = data['users']
+    # e = {int(k):v for k, v in d.items()}
+    # print(d)
+
+    #return make_response(jsonify({'users' : e}))
+
+
