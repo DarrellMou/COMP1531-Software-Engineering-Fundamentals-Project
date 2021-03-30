c6a8ca5369494c99df03bbaf0ddc8d0e62dccc45
Nikki <z5260192@ad.unsw.edu.au>
2021-03-30 21:05:45 +1100

    Working tests

--- tests/admin_user_remove_v1_test.py
+++ tests/admin_user_remove_v1_test.py
@@ -53,15 +53,20 @@ def test_admin_user_remove_only_channel_owner(setup_user):
 def test_admin_user_remove_only_channel_owner(setup_user):
     users = setup_user
 
-    # User 1 makes channel 1 and User 3 joins. User 1 sends a message
+    # User 1 makes channel 1 
     channel_id1 = channels_create_v2(users['user1']['token'],'Test Channel',True)
+    # User 3 joins the public channel
     channel_join_v2(users['user3']['token'], channel_id1['channel_id'])
+    # User 1 makes User 3 an owner
     channel_addowner_v1(users['user1']['token'], channel_id1['channel_id'], users['user3']['auth_user_id'])
+    # User 1 sends a message
     message_id1 = message_send_v2(users['user1']['token'],channel_id1['channel_id'],'Nice day')
 
-    # User 2 makes channel 2 and User 1 joins. User 1 sends a message
+    # User 2 makes channel 2 
     channel_id2 = channels_create_v2(users['user2']['token'],'Test Channel',False)
+    # User 1 joins the private channel as global owner
     channel_join_v2(users['user1']['token'], channel_id2['channel_id'])
+    # User 1 sends a message
     message_id2 = message_send_v2(users['user1']['token'],channel_id2['channel_id'],'Hello user2')
 
     # User 1 creates a dm to User 2 and User 3. User 1 sends a message
@@ -69,7 +74,7 @@ def test_admin_user_remove_only_channel_owner(setup_user):
     message_dm_id1 = message_senddm_v1(users['user1']['token'],dm_id1['dm_id'],'Hi guys')
 
     # Set up variables to test function outputs
-    #user_profile_id1 = user_profile_v2(users['user1']['token'],users['user1']['auth_user_id'])
+    # user_profile_id1 = user_profile_v2(users['user1']['token'],users['user1']['auth_user_id'])
     messages_channel_id1 = channel_messages_v2(users['user3']['token'],channel_id1['channel_id'],0)
     messages_channel_id2 = channel_messages_v2(users['user2']['token'],channel_id2['channel_id'],0)
     messages_dm_id_1 = dm_messages_v1(users['user2']['token'],dm_id1['dm_id'],0)
@@ -82,12 +87,13 @@ def test_admin_user_remove_only_channel_owner(setup_user):
     
     # Global User 2 removes Global User 1
     admin_userpermission_change_v1(users['user1']['token'], users['user2']['auth_user_id'], 1)
-    admin_userpermission_change_v1(users['user2']['token'], users['user3']['auth_user_id'], 1)    
+    admin_userpermission_change_v1(users['user2']['token'], users['user3']['auth_user_id'], 1)   
+    # Function called 
     admin_user_remove_v1(users['user2']['token'], users['user1']['auth_user_id'])
     
     # Ensure the correct output after calling admin_user_remove
     #assert user_profile_id1['auth_user_id'][0]['first_name'] == "Removed user"
     assert messages_channel_id1['messages'][0]['message'] == "Removed user"
     assert messages_channel_id2['messages'][0]['message'] == "Removed user"
-    assert messages_dm_id1['messages'][0]['message'] == "Removed user"
+    assert messages_dm_id_1['messages'][0]['message'] == "Removed user"
     
