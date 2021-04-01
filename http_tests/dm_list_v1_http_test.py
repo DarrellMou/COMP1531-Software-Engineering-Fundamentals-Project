ed15f71fdba9516ff1d6bb43292ae3f4fed4d8bf
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-04-02 04:45:49 +1100

    removed extra forward slash from url

--- http_tests/dm_list_v1_http_test.py
+++ http_tests/dm_list_v1_http_test.py
@@ -26,18 +26,18 @@ def dm_list_body(user):
     }
 
 def test_function():
-    requests.delete(f"{url}/clear/v1")
+    requests.delete(f"{url}clear/v1")
     
-    a_u_id0 = requests.post(f"{url}/auth/register/v2", json=user_body(0))
+    a_u_id0 = requests.post(f"{url}auth/register/v2", json=user_body(0))
     user0 = a_u_id0.json()
 
-    a_u_id1 = requests.post(f"{url}/auth/register/v2", json=user_body(1))
+    a_u_id1 = requests.post(f"{url}auth/register/v2", json=user_body(1))
     user1 = a_u_id1.json()
 
-    dm_id0 = requests.post(f"{url}/dm/create/v1", json=dm_create_body(user0, [user1]))
+    dm_id0 = requests.post(f"{url}dm/create/v1", json=dm_create_body(user0, [user1]))
     dm0 = dm_id0.json()
 
-    r = requests.get(f"{url}/dm/list/v1", json=dm_list_body(user0))
+    r = requests.get(f"{url}dm/list/v1", json=dm_list_body(user0))
     dm_list = r.json()
 
     assert dm_list == {
@@ -50,19 +50,19 @@ def test_function():
     }
 
 def test_multiple():
-    requests.delete(f"{url}/clear/v1")
+    requests.delete(f"{url}clear/v1")
 
     users = []
     for i in range(5):
-        a_u_id = requests.post(f"{url}/auth/register/v2", json=user_body(i))
+        a_u_id = requests.post(f"{url}auth/register/v2", json=user_body(i))
         users.append(a_u_id.json())
 
     dms = []
     for i in range(4):
-        dm_id = requests.post(f"{url}/dm/create/v1", json=dm_create_body(users[0], [users[i + 1]]))
+        dm_id = requests.post(f"{url}dm/create/v1", json=dm_create_body(users[0], [users[i + 1]]))
         dms.append(dm_id.json())
 
-    r = requests.get(f"{url}/dm/list/v1", json=dm_list_body(users[0]))
+    r = requests.get(f"{url}dm/list/v1", json=dm_list_body(users[0]))
     dm_list = r.json()
 
     assert dm_list == {
@@ -87,9 +87,9 @@ def test_multiple():
     }
 
 def test_invalid_token():
-    requests.delete(f"{url}/clear/v1")
+    requests.delete(f"{url}clear/v1")
 
-    r = requests.get(f"{url}/dm/list/v1", json=dm_list_body({"token": 513875017835}))
+    r = requests.get(f"{url}dm/list/v1", json=dm_list_body({"token": 513875017835}))
     dm_list = r.json()
 
     assert dm_list["code"] == 403
