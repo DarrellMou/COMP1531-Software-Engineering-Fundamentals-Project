95264c346c8cdc2f302b1a3ae4bda7a9bb11224f
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-04-02 06:54:59 +1100

    Added tests for dm_leave

---
+++ http_tests/dm_leave_v1_http_test.py
@@ -0,0 +1,143 @@
+import json
+import requests
+import urllib
+
+from src.config import url
+# HELPER FUNCTIONS
+
+def user_body(num):
+    return {
+        "email": f"example{num}@hotmail.com",
+        "password": f"password{num}",
+        "name_first": f"first_name{num}",
+        "name_last": f"last_name{num}"
+    }
+
+def dm_create_body(user, u_ids): 
+    u_ids_list = [u_id['auth_user_id'] for u_id in u_ids]
+    return {
+        "token": user["token"],
+        "u_ids": u_ids_list
+    }
+
+def dm_details_body(user, dm):
+    return {
+        "token": user["token"],
+        "dm_id": dm["dm_id"]
+    }
+
+def dm_leave_body(user, dm):
+    return {
+        "token": user["token"],
+        "dm_id": dm["dm_id"]
+    }
+
+def test_function():
+    requests.delete(f"{url}clear/v1")
+
+    users = []
+    for i in range(3):
+        a_u_id = requests.post(f"{url}auth/register/v2", json=user_body(i))
+        users.append(a_u_id.json())
+
+    dm_id0 = requests.post(f"{url}dm/create/v1", json=dm_create_body(users[0], [user[1], user[2]]))
+    dm0 = dm_id0.json()
+
+    requests.post(f"{url}dm/leave/v1", json=dm_leave_body(users[2], dm0))
+
+    payload = requests.get(f"{url}dm/details/v1", json=dm_details_body(users[0], dm0))
+    dm_details = payload.json()
+
+    assert dm_details == {
+        'name': 'first_name0last_name, first_name1last_name',
+        'members': [
+            {
+                'u_id': users[0]['auth_user_id'],
+                'name_first': 'first_name1',
+                'name_last': 'last_name1',
+            },
+            {
+                'u_id': users[1]['auth_user_id'],
+                'name_first': 'first_name3',
+                'name_last': 'last_name3',
+            }
+        ]
+    }
+
+def test_multiple():
+    requests.delete(f"{url}clear/v1")
+
+    users = []
+    for i in range(5):
+        a_u_id = requests.post(f"{url}auth/register/v2", json=user_body(i))
+        users.append(a_u_id.json())
+
+    dm_id0 = requests.post(f"{url}dm/create/v1", json=dm_create_body(users[0], [users[1], users[2], users[3], users[4]]))
+    dm0 = dm_id0.json()
+
+    for i in range(4):
+        requests.post(f"{url}dm/leave/v1", json=dm_leave_body(users[i + 1], dm0))
+
+    payload = requests.get(f"{url}dm/details/v1", json=dm_details_body(users[0], dm0))
+    dm_details = payload.json()
+
+    assert dm_details == {
+        'name': ' first_name0last_name, first_name1last_name, first_name2last_name, first_name3last_name, first_name4last_name',
+        'members': [
+            {
+                'u_id': users[0]['auth_user_id'],
+                'name_first': 'first_name0',
+                'name_last': 'last_name0',
+            }
+        ]
+    }
+
+def test_invalid_token():
+    requests.delete(f"{url}clear/v1")
+    
+    a_u_id0 = requests.post(f"{url}auth/register/v2", json=user_body(0))
+    user0 = a_u_id0.json()
+
+    a_u_id1 = requests.post(f"{url}auth/register/v2", json=user_body(1))
+    user1 = a_u_id1.json()
+
+    dm_id0 = requests.post(f"{url}dm/create/v1", json=dm_create_body(user0, [user1]))
+    dm0 = dm_id0.json()
+
+    r = requests.post(f"{url}dm/leave/v1", json=dm_leave_body({"token": 573105791532}, dm0))
+    response = r.json()
+
+    assert response["code"] == 403
+    assert response["name"] == "System Error"
+    assert response["message"] == "<p></p>
+
+def test_invalid_dm_id():
+    requests.delete(f"{url}clear/v1")
+    
+    a_u_id0 = requests.post(f"{url}auth/register/v2", json=user_body(0))
+    user0 = a_u_id0.json()
+
+    r = requests.post(f"{url}dm/leave/v1", json=dm_leave_body(user0, {"dm_id": 53120246942}))
+    response = r.json()
+
+    assert response["code"] == 400
+    assert response["name"] == "System Error"
+    assert response["message"] == "<p></p>
+
+def test_unauthorised_user():
+    requests.delete(f"{url}clear/v1")
+
+    users = []
+    for i in range(3):
+        a_u_id = requests.post(f"{url}auth/register/v2", json=user_body(i))
+        users.append(a_u_id.json())
+
+    dm_id0 = requests.post(f"{url}dm/create/v1", json=dm_create_body(users[0], [user[1]]))
+    dm0 = dm_id0.json()
+
+    r = requests.post(f"{url}dm/leave/v1", json=dm_leave_body(users[2], dm0))
+    response = r.json()
+
+    assert response["code"] == 403
+    assert response["name"] == "System Error"
+    assert response["message"] == "<p></p>
