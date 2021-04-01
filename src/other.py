f0c5038bb5cf91704811e2ac0e01c92f2b2924f0
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-04-01 23:03:16 +1100

    fixed merge conflict

--- src/other.py
+++ src/other.py
@@ -13,12 +13,8 @@ def clear_v1():
     data = {
         "users" : {},
         "channels" : {},
-<<<<<<< HEAD
         "dms" : {},
         "messages" : []
-=======
-        "dms": {}
->>>>>>> darrell/dm_details_v1
     }
     with open("data.json", "w") as FILE:
         json.dump(data, FILE)
