8a3586a8e77990af938b63a8db5a07531f75d6c6
Nikki <z5260192@ad.unsw.edu.au>
2021-04-02 14:46:57 +1100

    Tests still not working

--- http_tests/message_senddm_http_test.py
+++ http_tests/message_senddm_http_test.py
@@ -5,15 +5,21 @@ import requests
 import pytest
 from src import config
 
+def dm_create_body(user, u_ids): 
+    u_ids_list = [u_id['auth_user_id'] for u_id in u_ids]
+    return {
+        'token': user["token"],
+        'u_ids': u_ids_list
+    }
+
 def test_channels_create_access_error(setup_user_data):
     users = setup_user_data
 
     # Creating a dm
-    u_id_list = [users['user2']['token'],users['user3']['token']]
-    dm_id1 = requests.post(config.url + '/dm/create/v1', json={
-        'token': users['user1']['token'],
-        'u_id': u_id_list,
-    }).json()
+    u_id_list = [users['user2'],users['user3']]
+    dm_id1 = requests.post(config.url + '/dm/create/v1', json=dm_create_body(users['user1'],u_id_list)).json()
+
+    print(dm_id1)
 
     # Invalidate an existing token to guarantee a token is invalid 
     invalid_token = users['user1']['token']
@@ -24,7 +30,7 @@ def test_channels_create_access_error(setup_user_data):
     # Ensure AccessError
     assert requests.post(config.url + '/message/senddm/v1', json={
         'token': invalid_token,
-        'dm_id': dm_id1['dm_id'],
+        'dm_id': dm_id1,
         'message': "Hello",
     }).status_code == 403
 
@@ -33,7 +39,7 @@ def test_channels_create_input_error(setup_user_data):
     users = setup_user_data
 
     # Creating a dm
-    u_id_list = [users['user2']['token'],users['user3']['token']]
+    u_id_list = [users['user2']['auth_user_id'],users['user3']['auth_user_id']]
     dm_id1 = requests.post(config.url + '/dm/create/v1', json={
         'token': users['user1']['token'],
         'u_id': u_id_list,
