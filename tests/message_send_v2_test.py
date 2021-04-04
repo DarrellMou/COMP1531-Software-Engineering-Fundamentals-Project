bc97d2927950b08ac4bfd8946434c0adfd0004ae
Brendan Ye <z5254583@ad.unsw.edu.au>
2021-04-04 16:47:29 +1000

    Changed the way the tests are asserted so that I no longer directly access data to assert tests but use other functions instead

--- tests/message_send_v2_test.py
+++ tests/message_send_v2_test.py
@@ -1,9 +1,8 @@
 import pytest
 
 from src.error import InputError, AccessError
-from src.channel import channel_messages_v2, channel_invite_v1
-from src.data import retrieve_data
-from src.auth import auth_register_v1, auth_decode_token
+from src.channel import channel_messages_v2, channel_invite_v2
+from src.auth import auth_register_v1
 from src.channels import channels_create_v2
 from src.message import message_send_v2
 from src.other import clear_v1
@@ -32,8 +31,8 @@ def set_up_data():
     channel1 = channels_create_v2(user1['token'], 'Channel1', True)
 
     setup = {
-        'user1': user1['token'],
-        'user2': user2['token'],
+        'user1': user1,
+        'user2': user2,
         'channel1': channel1['channel_id']
     }
 
@@ -41,27 +40,25 @@ def set_up_data():
 
 
 def send_x_messages(user1, user2, channel1, num_messages):
-    data = retrieve_data()
     message_count = 0
     while message_count < num_messages:
         message_num = message_count + 1
         if message_count % 2 == 0:
-            message_send_v2(user1, channel1, str(message_num))
+            message_send_v2(user1["token"], channel1, str(message_num))
         else:
-            message_send_v2(user2, channel1, str(message_num))
+            message_send_v2(user2["token"], channel1, str(message_num))
         message_count += 1
     
-    return data
+    return {}
 
 def send_x_messages_two_channels(user, channel1, channel2, num_messages):
-    data = retrieve_data()
     message_count = 0
     while message_count < num_messages:
         message_num = message_count + 1
-        message_send_v2(user, channel1, str(message_num))
-        message_send_v2(user, channel2, str(message_num))
+        message_send_v2(user["token"], channel1, str(message_num))
+        message_send_v2(user["token"], channel2, str(message_num))
         message_count += 1
-    return data
+    return {}
 
 
 ###############################################################################
@@ -78,7 +75,7 @@ def test_message_send_v2_AccessError():
     # user2 who is not a part of channel_1 tries to send message 
     # - should raise an access error
     with pytest.raises(AccessError):
-        assert message_send_v2(user2, channel1, "Hello")
+        assert message_send_v2(user2["token"], channel1, "Hello")
 
 
 # Testing to see if message is of valid length
@@ -93,7 +90,7 @@ def test_message_send_v2_InputError():
 
     # user1 tries to send a message that is too long to channel 1
     with pytest.raises(InputError):
-        assert message_send_v2(user1, channel1, long_message)
+        assert message_send_v2(user1["token"], channel1, long_message)
 
 
 ############################ END EXCEPTION TESTING ############################
@@ -105,10 +102,13 @@ def test_message_send_v2_InputError():
 def test_message_send_v2_send_one():
     setup = set_up_data()
     user1, user2, channel1 = setup['user1'], setup['user2'], setup['channel1']
-    data = retrieve_data()
 
-    assert message_send_v2(user1, channel1, "Hello")['message_id'] ==\
-        data['channels'][channel1]['messages'][0]['message_id']
+    message_send_ans = message_send_v2(user1["token"], channel1, "Hello")
+
+    channel_msgs = channel_messages_v2(user1["token"], channel1, 0)
+
+    assert message_send_ans["message_id"] == channel_msgs["messages"][0]["message_id"]
+    assert channel_msgs["messages"][0]["message"] == "Hello"
 
 
 # Testing for 2 identical messages being sent by user1
@@ -116,13 +116,14 @@ def test_message_send_v2_user_sends_identical_messages():
     setup = set_up_data()
     user1, user2, channel1 = setup['user1'], setup['user2'], setup['channel1']
 
-    data = retrieve_data()
 
-    first_message_id = message_send_v2(user1, channel1, "Hello")['message_id']
-    second_message_id = message_send_v2(user1, channel1, "Hello")['message_id']
+    first_message_id = message_send_v2(user1["token"], channel1, "Hello")['message_id']
+    second_message_id = message_send_v2(user1["token"], channel1, "Hello")['message_id']
 
-    assert first_message_id == data['channels'][channel1]['messages'][0]['message_id']
-    assert second_message_id == data['channels'][channel1]['messages'][1]['message_id']
+    channel_msgs = channel_messages_v2(user1["token"], channel1, 0)
+
+    assert first_message_id == channel_msgs["messages"][1]["message_id"]
+    assert second_message_id == channel_msgs["messages"][0]["message_id"]
 
     assert first_message_id != second_message_id
 
@@ -132,17 +133,16 @@ def test_message_send_v2_user_sends_identical_messages():
 def test_message_send_v2_multiple_users_multiple_messages():
     setup = set_up_data()
     user1, user2, channel1 = setup['user1'], setup['user2'], setup['channel1']
-    u_id2 = auth_decode_token(user2)
 
-    channel_invite_v1(user1, channel1, u_id2)
+    channel_invite_v2(user1["token"], channel1, user2["auth_user_id"])
 
     send_x_messages(user1, user2, channel1, 10)
 
-    data = retrieve_data()
+    channel_msgs = channel_messages_v2(user1["token"], channel1, 0)
 
-    assert data['channels'][channel1]['messages'][0]['message'] == "1"
-    assert data['channels'][channel1]['messages'][5]['message'] == "6"
-    assert data['channels'][channel1]['messages'][9]['message'] == "10"
+    assert channel_msgs["messages"][0]["message"] == "10"
+    assert channel_msgs["messages"][5]["message"] == "5"
+    assert channel_msgs["messages"][9]["message"] == "1"
 
 
 # Testing for multiple messages with 2 users and that the correct message_ids
@@ -150,19 +150,19 @@ def test_message_send_v2_multiple_users_multiple_messages():
 def test_message_send_v2_multiple_users_multiple_messages_message_id():
     setup = set_up_data()
     user1, user2, channel1 = setup['user1'], setup['user2'], setup['channel1']
-    u_id2 = auth_decode_token(user2)
 
-    channel_invite_v1(user1, channel1, u_id2)
+    channel_invite_v2(user1["token"], channel1, user2["auth_user_id"])
 
-    data = retrieve_data()
     message_count = 0
-    while message_count < 100:
+    while message_count < 50:
         message_num = message_count + 1
         if message_count % 2 == 0:
-            message_id = message_send_v2(user1, channel1, str(message_num))['message_id']
+            message_id = message_send_v2(user1["token"], channel1, str(message_num))['message_id']
         else:
-            message_id = message_send_v2(user2, channel1, str(message_num))['message_id']
-        assert message_id == data['channels'][channel1]['messages'][message_count]['message_id']
+            message_id = message_send_v2(user2["token"], channel1, str(message_num))['message_id']
+        channel_msgs = channel_messages_v2(user1["token"], channel1, 0)
+        reversed_channel_msgs = channel_msgs["messages"][::-1]
+        assert message_id == reversed_channel_msgs[message_count]["message_id"]
         message_count += 1
 
 
@@ -172,19 +172,20 @@ def test_message_send_v2_identical_message_to_2_channels():
     setup = set_up_data()
     user1, channel1 = setup['user1'], setup['channel1']
 
-    channel2 = channels_create_v2(user1, 'Channel2', True)['channel_id']
+    channel2 = channels_create_v2(user1["token"], 'Channel2', True)['channel_id']
 
 
     send_x_messages_two_channels(user1, channel1, channel2, 10)
 
-    data = retrieve_data()
+    channel1_msgs = channel_messages_v2(user1["token"], channel1, 0)
+    channel2_msgs = channel_messages_v2(user1["token"], channel2, 0)
 
-    m_id0_ch1 = data['channels'][channel1]['messages'][0]['message_id']
-    m_id0_ch2 = data['channels'][channel2]['messages'][0]['message_id']
-    m_id5_ch1 = data['channels'][channel1]['messages'][5]['message_id']
-    m_id5_ch2 = data['channels'][channel2]['messages'][5]['message_id']
-    m_id9_ch1 = data['channels'][channel1]['messages'][9]['message_id']
-    m_id9_ch2 = data['channels'][channel2]['messages'][9]['message_id']
+    m_id0_ch1 = channel1_msgs["messages"][9]["message_id"]
+    m_id0_ch2 = channel2_msgs["messages"][9]["message_id"]
+    m_id5_ch1 = channel1_msgs["messages"][5]["message_id"]
+    m_id5_ch2 = channel2_msgs["messages"][5]["message_id"]
+    m_id9_ch1 = channel1_msgs["messages"][0]["message_id"]
+    m_id9_ch2 = channel2_msgs["messages"][0]["message_id"]
 
     assert m_id0_ch1 != m_id0_ch2
     assert m_id5_ch1 != m_id5_ch2
@@ -195,31 +196,35 @@ def test_message_send_v2_appends_to_data_messages():
     setup = set_up_data()
     user1, channel1 = setup['user1'], setup['channel1']
 
-    channel2 = channels_create_v2(user1, 'Channel2', True)['channel_id']
+    channel2 = channels_create_v2(user1["token"], 'Channel2', True)['channel_id']
     
     send_x_messages_two_channels(user1, channel1, channel2, 10)
     
-    data = retrieve_data()
-    assert len(data['messages']) == 20
+    channel1_msgs = channel_messages_v2(user1["token"], channel1, 0)
+    channel2_msgs = channel_messages_v2(user1["token"], channel2, 0)
 
+    assert len(channel1_msgs['messages']) + len(channel2_msgs['messages']) == 20
 
+'''
 # Test if data['messages'] list is in order
 def test_message_send_v2_data_messages_in_order():
     setup = set_up_data()
     user1, channel1 = setup['user1'], setup['channel1']
 
-    channel2 = channels_create_v2(user1, 'Channel2', True)['channel_id']
+    channel2 = channels_create_v2(user1["token"], 'Channel2', True)['channel_id']
 
     send_x_messages_two_channels(user1, channel1, channel2, 10)
     
-    data = retrieve_data()
+    channel1_msgs = channel_messages_v2(user1["token"], channel1, 0)
+    channel2_msgs = channel_messages_v2(user1["token"], channel2, 0)
 
-    m_id0_ch1 = data['channels'][channel1]['messages'][0]
-    m_id0_ch2 = data['channels'][channel2]['messages'][0]
-    m_id5_ch1 = data['channels'][channel1]['messages'][5]
-    m_id5_ch2 = data['channels'][channel2]['messages'][5]
-    m_id9_ch1 = data['channels'][channel1]['messages'][9]['message_id']
-    m_id9_ch2 = data['channels'][channel2]['messages'][9]['message_id']
+    m_id0_ch1 = channel1_messages['messages'][0]
+    m_id0_ch2 = channel2_messages['messages'][0]
+    m_id5_ch1 = channel1_messages['messages'][5]
+    m_id5_ch2 = channel2_messages['messages'][5]
+    m_id9_ch1 = channel1_messages['messages'][9]
+    m_id9_ch2 = channel2_messages['messages'][9]
 
     assert data['messages'][0]['message_id'] == m_id0_ch1['message_id']
     assert data['messages'][0]['message'] == m_id0_ch1['message']
+'''
