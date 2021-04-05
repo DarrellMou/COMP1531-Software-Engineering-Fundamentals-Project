3843a2aac1e47d210ea36afae959ea6a8cfd37b9
Nikki Yao <n.yao@student.unsw.edu.au>
2021-04-06 00:07:06 +1000

    Update docstring format

--- src/notifications.py
+++ src/notifications.py
@@ -4,15 +4,21 @@
 from src.data import data, retrieve_data
 from src.auth import auth_decode_token
 
-# Accesses the 20 most recent notifications of a user
-# Arguments
-#   token (int) The login session of the person accessing their notifications that were triggered by other functions
-# Exceptions
-#   Input Error - N/A
-#   AccessError - N/A
-# Return value
-#   notifications (list of notification data structures) - A list of notifications that the user has recieved
 def notifications_get_v1(token):
+    '''
+    Accesses the 20 most recent notifications of a user
+    
+    Arguments:
+        token (int) The login session of the person accessing their notifications that were triggered by other functions
+
+    Exceptions:
+        Input Error - N/A
+        AccessError - N/A
+        
+    Return value:
+        notifications (list of notification data structures) - A list of notifications that the user has recieved
+    '''
+
     data = retrieve_data()
     user_id = auth_decode_token(token)
 
