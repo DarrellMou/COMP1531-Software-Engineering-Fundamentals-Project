f597f0aea1774be4f3d27326b3b2d3c92baa6b3a
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-04-11 19:49:07 +1000

    Added pytest fixture for personal use

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,3 +35,15 @@ def setup_user(reset):
         'user4' : a_u_id4,
         'user5' : a_u_id5
     }
+
+@pytest.fixture
+def users(reset):
+
+    # a_u_id* has two fields: token and auth_user_id
+    a_u_id0 = auth_register_v1('user1@email.com', 'User1_pass!', 'user1_first', 'user1_last')
+    a_u_id1 = auth_register_v1('user2@email.com', 'User2_pass!', 'user2_first', 'user2_last')
+    a_u_id2 = auth_register_v1('user3@email.com', 'User3_pass!', 'user3_first', 'user3_last')
+    a_u_id3 = auth_register_v1('user4@email.com', 'User4_pass!', 'user4_first', 'user4_last')
+    a_u_id4 = auth_register_v1('user5@email.com', 'User5_pass!', 'user5_first', 'user5_last')
+
+    return [a_u_id0, a_u_id1, a_u_id2, a_u_id3, a_u_id4]
