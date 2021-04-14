ea14ebf62206e76add5a1e8a1e8de489c2736691
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-04-14 22:10:57 +1000

    Fixed tests

--- tests/standup_active_v1_test.py
+++ tests/standup_active_v1_test.py
@@ -11,18 +11,18 @@ import time
 
 def test_function_active(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
-    standup_start_v1(users[0]['token'], ch_id0['channel_id'], 5)
+    standup_start_v1(users[0]['token'], ch_id0['channel_id'], 1)
 
-    standup = standup_active_v1(users[0]['token'], ch_id0)
+    standup = standup_active_v1(users[0]['token'], ch_id0['channel_id'])
 
     assert standup["is_active"] == True
-    assert float("{:.2f}".format(standup["time_finish"])) == float("{:.2f}".format(datetime.now().timestamp() + 5))
-    time.sleep(6)
+    assert float("{:.2f}".format(standup["time_finish"])) == float("{:.2f}".format(datetime.now().timestamp() + 1))
+    time.sleep(2)
 
 def test_function_inactive(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
 
-    standup = standup_active_v1(users[0]['token'], ch_id0)
+    standup = standup_active_v1(users[0]['token'], ch_id0['channel_id'])
 
     assert standup["is_active"] == False
     assert standup["time_finish"] == None
@@ -30,22 +30,22 @@ def test_function_inactive(users):
 def test_multiple_runs(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
     standup_start_v1(users[0]['token'], ch_id0['channel_id'], 1)
-    standup = standup_active_v1(users[0]['token'], ch_id0)
+    standup = standup_active_v1(users[0]['token'], ch_id0['channel_id'])
 
     assert standup["is_active"] == True
     assert float("{:.2f}".format(standup["time_finish"])) == float("{:.2f}".format(datetime.now().timestamp() + 1))
     time.sleep(2)
 
 
-    standup_start_v1(users[1]['token'], ch_id0['channel_id'], 3)
-    standup = standup_active_v1(users[1]['token'], ch_id0)
+    standup_start_v1(users[0]['token'], ch_id0['channel_id'], 3)
+    standup = standup_active_v1(users[1]['token'], ch_id0['channel_id'])
 
     assert standup["is_active"] == True
     assert float("{:.2f}".format(standup["time_finish"])) == float("{:.2f}".format(datetime.now().timestamp() + 3))
     time.sleep(4)
 
-    standup_start_v1(users[2]['token'], ch_id0['channel_id'], 5)
-    standup = standup_active_v1(users[2]['token'], ch_id0)
+    standup_start_v1(users[0]['token'], ch_id0['channel_id'], 5)
+    standup = standup_active_v1(users[2]['token'], ch_id0['channel_id'])
 
     assert standup["is_active"] == True
     assert float("{:.2f}".format(standup["time_finish"])) == float("{:.2f}".format(datetime.now().timestamp() + 5))
@@ -58,5 +58,4 @@ def test_invalid_channel_id(users):
 def test_invalid_token(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
     with pytest.raises(AccessError):
-        standup_active_v1(12345, ch_id0)
-
+        standup_active_v1(12345, ch_id0['channel_id'])
