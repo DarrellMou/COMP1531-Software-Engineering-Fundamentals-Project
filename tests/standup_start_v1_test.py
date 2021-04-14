c5055127d2ae8efd4583f234bb0d2a02c5dd09e9
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-04-14 21:39:34 +1000

    Fixed tests

--- tests/standup_start_v1_test.py
+++ tests/standup_start_v1_test.py
@@ -11,48 +11,51 @@ import time
 
 def test_function(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
-    time_finish = standup_start_v1(users[0]['token'], ch_id0['channel_id'], 30)
+    time_finish = standup_start_v1(users[0]['token'], ch_id0['channel_id'], 5)
 
-    assert time_finish == datetime.now()timestamp() + 30
+    assert float("{:.2f}".format(time_finish)) == float("{:.2f}".format(datetime.now().timestamp() + 5))
+    time.sleep(6)
 
 def test_multiple_runs(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
     time_finish1 = standup_start_v1(users[0]['token'], ch_id0['channel_id'], 5)
 
-    assert time_finish1 == datetime.now()timestamp() + 5
+    assert float("{:.2f}".format(time_finish1)) == float("{:.2f}".format(datetime.now().timestamp() + 5))
     
-    time.sleep(5)
+    time.sleep(6)
 
     time_finish2 = standup_start_v1(users[0]['token'], ch_id0['channel_id'], 10)
 
-    assert time_finish2 == 10
+    assert float("{:.2f}".format(time_finish2)) == float("{:.2f}".format(datetime.now().timestamp() + 10))
 
-    time.sleep(10)
+    time.sleep(11)
 
     time_finish3 = standup_start_v1(users[0]['token'], ch_id0['channel_id'], 20)
 
-    assert time_finish3 == 20
+    assert float("{:.2f}".format(time_finish3)) == float("{:.2f}".format(datetime.now().timestamp() + 20))
+
+    time.sleep(21)
 
 def test_invalid_channel_id(users):
-
     with pytest.raises(InputError):
-        standup_start_v1(users[0]['token'], 12345, 30)
+        standup_start_v1(users[0]['token'], 12345, 1)
 
 def test_active_standup(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
-    standup_start_v1(users[0]['token'], ch_id0['channel_id'], 30)
+    standup_start_v1(users[0]['token'], ch_id0['channel_id'], 1)
 
     with pytest.raises(InputError):
-        standup_start_v1(users[0]['token'], ch_id0['channel_id'], 30)
+        standup_start_v1(users[0]['token'], ch_id0['channel_id'], 1)
+    time.sleep(1)
 
 def test_unauthorized_user(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
 
     with pytest.raises(AccessError):
-        standup_start_v1(users[1]['token'], ch_id0['channel_id'], 30)
+        standup_start_v1(users[1]['token'], ch_id0['channel_id'], 1)
 
 def test_invalid_token(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
 
     with pytest.raises(AccessError):
-        standup_start_v1(12345, ch_id0['channel_id'], 30)
+        standup_start_v1(12345, ch_id0['channel_id'], 1)
