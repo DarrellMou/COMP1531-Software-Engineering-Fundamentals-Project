b66b02431ff0aa35d30dead83671acc88473d709
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-04-15 20:06:36 +1000

    Added more asserts for when standup is inactive

--- tests/standup_active_v1_test.py
+++ tests/standup_active_v1_test.py
@@ -9,7 +9,7 @@ from src.channels import channels_create_v2
 from datetime import datetime
 import time
 
-def test_function_active(users):
+def test_function(users):
     ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
     standup_start_v1(users[0]['token'], ch_id0['channel_id'], 1)
 
@@ -18,9 +18,6 @@ def test_function_active(users):
     assert standup["is_active"] == True
     assert standup["time_finish"] == int(datetime.now().timestamp() + 1)
     time.sleep(2)
-
-def test_function_inactive(users):
-    ch_id0 = channels_create_v2(users[0]['token'], "Channel0", True)
 
     standup = standup_active_v1(users[0]['token'], ch_id0['channel_id'])
 
@@ -36,6 +33,10 @@ def test_multiple_runs(users):
     assert standup["time_finish"] == int(datetime.now().timestamp() + 1)
     time.sleep(2)
 
+    standup = standup_active_v1(users[0]['token'], ch_id0['channel_id'])
+
+    assert standup["is_active"] == False
+    assert standup["time_finish"] == None
 
     standup_start_v1(users[0]['token'], ch_id0['channel_id'], 3)
     standup = standup_active_v1(users[1]['token'], ch_id0['channel_id'])
@@ -44,12 +45,22 @@ def test_multiple_runs(users):
     assert standup["time_finish"] == int(datetime.now().timestamp() + 3)
     time.sleep(4)
 
+    standup = standup_active_v1(users[1]['token'], ch_id0['channel_id'])
+
+    assert standup["is_active"] == False
+    assert standup["time_finish"] == None
+
     standup_start_v1(users[0]['token'], ch_id0['channel_id'], 5)
     standup = standup_active_v1(users[2]['token'], ch_id0['channel_id'])
 
     assert standup["is_active"] == True
     assert standup["time_finish"] == int(datetime.now().timestamp() + 5)
     time.sleep(6)
+
+    standup = standup_active_v1(users[2]['token'], ch_id0['channel_id'])
+
+    assert standup["is_active"] == False
+    assert standup["time_finish"] == None
 
 def test_invalid_channel_id(users):
     with pytest.raises(InputError):
