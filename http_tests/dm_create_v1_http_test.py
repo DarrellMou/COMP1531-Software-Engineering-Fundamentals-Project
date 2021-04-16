764e17ffc1d1fbbdd8092787589e0bab7af23fcb
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-04-16 15:11:56 +1000

    Tests now include email and handle_str

--- http_tests/dm_create_v1_http_test.py
+++ http_tests/dm_create_v1_http_test.py
@@ -52,15 +52,19 @@ def test_function():
         'members': [
             {
                 'u_id': user0['auth_user_id'],
+                'email': "example0@hotmail.com",
                 'name_first': 'first_name0',
                 'name_last': 'last_name0',
+                'handle_str': 'first_name0last_name',
             },
             {
                 'u_id': user1['auth_user_id'],
+                'email': "example1@hotmail.com",
                 'name_first': 'first_name1',
                 'name_last': 'last_name1',
-            }
-        ]
+                'handle_str': 'first_name1last_name',
+            },
+        ],
     }
 
 def test_multiple():
@@ -82,28 +86,38 @@ def test_multiple():
         'members': [
             {
                 'u_id': users[0]['auth_user_id'],
+                'email': "example0@hotmail.com",
                 'name_first': 'first_name0',
                 'name_last': 'last_name0',
+                'handle_str': 'first_name0last_name',
             },
             {
                 'u_id': users[1]['auth_user_id'],
+                'email': "example1@hotmail.com",
                 'name_first': 'first_name1',
                 'name_last': 'last_name1',
+                'handle_str': 'first_name1last_name',
             },
             {
                 'u_id': users[2]['auth_user_id'],
+                'email': "example2@hotmail.com",
                 'name_first': 'first_name2',
                 'name_last': 'last_name2',
+                'handle_str': 'first_name2last_name',
             },
             {
                 'u_id': users[3]['auth_user_id'],
+                'email': "example3@hotmail.com",
                 'name_first': 'first_name3',
                 'name_last': 'last_name3',
+                'handle_str': 'first_name3last_name',
             },
             {
                 'u_id': users[4]['auth_user_id'],
+                'email': "example4@hotmail.com",
                 'name_first': 'first_name4',
                 'name_last': 'last_name4',
+                'handle_str': 'first_name4last_name',
             },
         ]
     }
@@ -124,9 +138,11 @@ def test_empty_u_ids_list():
         'name': 'first_name0last_name',
         'members': [
             {
-                'u_id': user0['auth_user_id'],
+                'u_id': users[0]['auth_user_id'],
+                'email': "example0@hotmail.com",
                 'name_first': 'first_name0',
                 'name_last': 'last_name0',
+                'handle_str': 'first_name0last_name',
             },
         ],
     }
