d07aa2dff9511a43f3fd19c099cc81b40172e459
Darrell Mounarath <z5257091@ad.unsw.edu.au>
2021-04-16 14:52:31 +1000

    Added empty u_ids list check

--- tests/dm_details_v1_test.py
+++ tests/dm_details_v1_test.py
@@ -73,6 +73,23 @@ def test_multiple():
         ]
     }
 
+def test_empty_u_ids_list():
+    clear_v1()
+    a_u_id1 = auth_register_v1('example1@hotmail.com', 'password1', 'first_name1', 'last_name1')
+
+    dm_id = dm_create_v1(a_u_id1['token'], [])
+
+    assert dm_details_v1(a_u_id1['token'], dm_id['dm_id']) == {
+        'name': 'first_name1last_name',
+        'members': [
+            {
+                'u_id': a_u_id1['auth_user_id'],
+                'name_first': 'first_name1',
+                'name_last': 'last_name1',
+            },
+        ],
+    }
+
 # dm_details given invalid dm_id
 def test_invalid_dm_id():
     clear_v1()
