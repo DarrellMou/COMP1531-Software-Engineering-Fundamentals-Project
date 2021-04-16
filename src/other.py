4df719407515e78ea2ecb7b43e95d299f11713b2
Nikki <z5260192@ad.unsw.edu.au>
2021-04-16 21:51:06 +1000

    Removed exception raise assumption that the only owner of Dreams cannot switch their permission to member

--- src/other.py
+++ src/other.py
@@ -206,6 +206,7 @@ def admin_userpermission_change_v1(token, u_id, permission_id):
     # Checks if permission_id refers to a value permission
     if not (permission_id == 1 or permission_id == 2): raise InputError("Not a value permission")
 
+    ''' * Removed as Exception is not raised in the given spec *
     # Checks if the user is the currently the only owner and wants to be changed to a member
     if permission_id == 2 and auth_user_id == u_id:
         admin_flag = 0
@@ -213,6 +214,7 @@ def admin_userpermission_change_v1(token, u_id, permission_id):
             if data['users'][user]['permission_id'] == 1:
                 admin_flag += 1
         if admin_flag <= 1: raise InputError("The user is currently the only global owner")
+    '''
 
     # Change u_id permission to permission_id
     data['users'][u_id]['permission_id'] = permission_id
